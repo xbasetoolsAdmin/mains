bd8aacc8bd60d16f1cfec0d409ee7985d88e2425
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-25 16:28:53 -0700

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,3 @@
 <?php
-
-
-$dbcon = mysqli_connect("localhost","artmir","Omeri1233","artmir") or die('enable connection'); 
-
-
-
+$dbcon = mysqli_connect("localhost","xbaseleets_xbaseleets","xbaseleets_xbaseleets","xbaseleets_xbaseleets") or die('enable connection'); 
 ?>
