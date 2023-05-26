cc2d54d34f94d55332c5350eb24c28d03e18f272
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-26 02:52:40 +0100

    Update cPanel.php

--- cPanel.php
+++ cPanel.php
@@ -409,8 +409,12 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
             </div>
         </div>
         <div class="alert alert-info text-left" role="alert" style="margin: 15px;">
-            
-            
+                  <ul>
+            <li>We Have Fresh E-mails Lists for Diffrents Countries ( You Can Use it for Spam ). </li>
+            <li>Our E-mails Lists Guarante for you Good results because most of them are from Shops and Big Sites .</li>
+            <li>Combo use for cracking (Brute forcing attack) not use for login to email.</li>
+            <li>There is <b> 0 </b> Available.</li>
+        </ul>
         </div>
             <div class="row m-2 pt-3" style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
                 <div class="col-sm-12 table-responsive">
