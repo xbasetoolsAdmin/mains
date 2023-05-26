ae99bbbdeb0fd0d58071a1ae9a59349c0aa71d82
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-25 17:14:41 -0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -36,6 +36,7 @@
     </div>
   </div>
 </div>
+                                                                          
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
