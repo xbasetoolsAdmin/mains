719b53803c8badf8d212be96c12eb17a40d5bec1
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-26 23:56:17 +0100

    Update addBalance.php

--- addBalance.php
+++ addBalance.php
@@ -152,7 +152,7 @@ $(window).on("popstate", function(e) {
 
 $(window).on('load', function() {
 $('.dropdown').hover(function(){ $('.dropdown-toggle', this).trigger('click'); });
-   pageDiv(13,'Add Balance - JeruxShop','addBalance.html',1);
+   pageDiv(13,'Add Balance - JeruxShop','addBalance',1);
    var clipboard = new Clipboard('.copyit');
     clipboard.on('success', function(e) {
       setTooltip(e.trigger, 'Copied!');
