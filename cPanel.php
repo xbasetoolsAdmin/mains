852f504270f60d84c55020ccf8c47a0c66b0462c
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-26 09:25:10 +0100

    Update cPanel.php

--- cPanel.php
+++ cPanel.php
@@ -450,23 +450,8 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
     }
 </style>
         <div class="alert alert-info text-left" role="alert" style="margin: 15px;">
+
         </div>
-            <div class="row m-2 pt-3" style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
-                <div class="col-sm-12 table-responsive">
-                    <table id="cpanel_table" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);" ">
-<?php
-ob_start();
-session_start();
-date_default_timezone_set('UTC');
-include "../includes/config.php";
-
-if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
-    header("location: ../");
-    exit();
-}
-$usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
-?>
-
 <ul class="nav nav-tabs">
   <li class="active"><a href="#filter" data-toggle="tab">Filter</a></li>
 </ul>
@@ -496,9 +481,12 @@ $query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `cpanels` WHERE
 </div>
 
 
-<table width="100%"  class="table table-striped table-bordered table-condensed sticky-header" id="table">
-<thead>
-    <tr>
+
+            <div class="row m-2 pt-3" style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
+                <div class="col-sm-12 table-responsive">
+                    <table id="cpanel_table" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);" ">
+                        <thead>
+                      <tr>
       <th scope="col" >Country</th>
       <th scope="col">TLD</th>
       <th scope="col">Detect Hosting</th>
