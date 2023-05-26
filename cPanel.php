9d7613b7ed2a2fd2d055fc3c6ee3c43efb4704eb
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-26 08:57:59 +0100

    Update cPanel.php

--- cPanel.php
+++ cPanel.php
@@ -457,9 +457,7 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
                         </div>
                     </div>
                 </div>
-                
-                                </div>
- 
+             </div>
                 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
@@ -587,86 +585,67 @@ $('.dropdown').hover(function(){ $('.dropdown-toggle', this).trigger('click'); }
                             $(btn).tooltip('hide'); /*console.log("hide-2");*/
                         }, 1000);
                     }
-                </script>
-    </body>
- 
-</html>
-<script type="text/javascript">
-    $('#filterbutton').click(function() {
-        $("#table tbody tr").each(function() {
-            var ck1 = $.trim($(this).find("#cpanel_country").text().toLowerCase());
-            var ck2 = $.trim($(this).find("#cpanel_tld").text().toLowerCase());
-            var ck3 = $.trim($(this).find("#cpanel_hosting").text().toLowerCase());
-            var ck4 = $.trim($(this).find("#cpanel_seller").text().toLowerCase());
-            var val1 = $.trim($('select[name="cpanel_country"]').val().toLowerCase());
-            var val2 = $.trim($('input[name="cpanel_tld"]').val().toLowerCase());
-            var val3 = $.trim($('input[name="cpanel_hosting"]').val().toLowerCase());
-            var val4 = $.trim($('select[name="cpanel_seller"]').val().toLowerCase());
-            if ((ck1 != val1 && val1 != '') || ck2.indexOf(val2) == -1 || ck3.indexOf(val3) == -1 || (ck4 != val4 && val4 != '')) {
-                $(this).hide();
-            } else {
-                $(this).show();
-            }
-        });
-        $('#filterbutton').prop('disabled', true);
-    });
-    $('.filterselect').change(function() {
-        $('#filterbutton').prop('disabled', false);
-    });
-    $('.filterinput').keyup(function() {
-        $('#filterbutton').prop('disabled', false);
-    });
- 
-    function buythistool(id) {
-        bootbox.confirm("Are you sure?", function(result) {
-            if (result == true) {
-                $.ajax({
-                    method: "GET",
-                    url: "buytool.php?id=" + id + "&t=cpanels",
-                    dataType: "text",
-                    success: function(data) {
-                        if (data.match(/<button/)) {
-                            $("#cpanel" + id).html(data).show();
-                        } else {
-                            bootbox.alert('<center><img src="files/img/balance.png"><h2><b>No enough balance !</b></h2><h4>Please refill your balance <a class="btn btn-primary btn-xs"  href="addBalance.html" onclick="window.open(this.href);return false;" >Add Balance <span class="glyphicon glyphicon-plus"></span></a></h4></center>')
-                        }
-                    },
-                });;
-            }
-        });
-    }
-    g: xcheck = 0;
- 
-    function check(id) {
-        if (xcheck > 1) {
-            bootbox.alert("<b>Wait</b> - Other checking operation is executed!");
-        } else {
-            xcheck++;
-            var type = $("#shop" + id).attr('type')
-            $("#shop" + id).html('Checking...').show();
-            $.ajax({
-                type: 'GET',
-                url: 'CheckCpanel' + id + '.html',
-                success: function(data) {
-                    $("#shop" + id).html(data).show();
-                    xcheck--;
-                }
-            });
-        }
-    }
- 
-    function openitem(order) {
-        $("#myModalLabel").text('Order #' + order);
-        $('#myModal').modal('show');
-        $.ajax({
-            type: 'GET',
-            url: 'showOrder' + order + '.html',
-            success: function(data) {
-                $("#modelbody").html(data).show();
-            }
-        });
- 
-    }
+    
+         $(document).ready(function() {
+         $('#cpanel_table').DataTable({
+            "lengthMenu": [
+                           [10, 25, 100, 500, -1],
+                           [10, 25, 100, 500, "All"]
+                            ],
+          'iDisplayLength': 1000,
+           "aaSorting": []
+                        });
+                    });
+$('#filterbutton').click(function () {$("#table tbody tr").each(function() {var ck1 = $.trim( $(this).find("#country").text().toLowerCase() );var ck2 = $.trim( $(this).find("#tld").text().toLowerCase() );var ck3 = $.trim( $(this).find("#hosting").text().toLowerCase() );var ck4 = $.trim( $(this).find("#seller").text().toLowerCase() ); var val1 = $.trim( $('select[name="country"]').val().toLowerCase() );var val2 = $.trim( $('input[name="tld"]').val().toLowerCase() );var val3 = $.trim( $('input[name="hosting"]').val().toLowerCase() );var val4 = $.trim( $('select[name="seller"]').val().toLowerCase() ); if((ck1 != val1 && val1 != '' ) || ck2.indexOf(val2)==-1 || ck3.indexOf(val3)==-1 || (ck4 != val4 && val4 != '' )){ $(this).hide();  }else{ $(this).show(); } });$('#filterbutton').prop('disabled', true);});$('.filterselect').change(function () {$('#filterbutton').prop('disabled', false);});$('.filterinput').keyup(function () {$('#filterbutton').prop('disabled', false);});
+function buythistool(id){
+  bootbox.confirm("Are you sure?", function(result) {
+        if(result ==true){
+      $.ajax({
+     method:"GET",
+     url:"buytool.php?id="+id+"&t=cpanels",
+     dataType:"text",
+     success:function(data){
+         if(data.match(/<button/)){
+		 $("#cpanel"+id).html(data).show();
+         }else{
+            bootbox.alert('<center><img src="files/img/balance.png"><h2><b>No enough balance !</b></h2><h4>Please refill your balance <a class="btn btn-primary btn-xs"  href="addBalance.html" onclick="window.open(this.href);return false;" >Add Balance <span class="glyphicon glyphicon-plus"></span></a></h4></center>')
+         }
+     },
+   });
+       ;}
+  });
+}
+g:xcheck=0;
+function check(id){   
+     if(xcheck > 1){
+    bootbox.alert("<b>Wait</b> - Other checking operation is executed!");
+  } else {
+    xcheck++;
+    var type = $("#shop"+id).attr('type')
+	$("#shop"+id).html('Checking...').show();
+	$.ajax({
+	type: 		'GET',
+	url: 		'CheckCpanel'+id+'.html',
+	success:	function(data)
+	{
+		$("#shop"+id).html(data).show();
+		xcheck--;
+	}});
+} }
+
+function openitem(order){
+  $("#myModalLabel").text('Order #'+order);
+  $('#myModal').modal('show');
+  $.ajax({
+    type:       'GET',
+    url:        'showOrder'+order+'.html',
+    success:    function(data)
+    {
+        $("#modelbody").html(data).show();
+    }});
+
+}
+
 </script>
 </body>
  
