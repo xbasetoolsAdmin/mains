87619f6d045c318bc198717e415093088ecb8c46
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-26 02:18:58 +0100

    Update cPanel.php

--- cPanel.php
+++ cPanel.php
@@ -3,7 +3,24 @@
 
 
 
-<div id="mainDiv">
+
+<div class="row m-2 pt-3" style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
+<div class="col-sm-12 table-responsive">
+<table id="mainDiv" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);" ">
+<thead>
+    <tr>
+      <th scope="col" >Country</th>
+      <th scope="col">TLD</th>
+      <th style="width: 7%" scope="col">SSL</th>
+      <th scope="col">Detected Hosting</th>
+      <th scope="col">Seller</th>
+      <th style="width: 7%" scope="col">Check</th>
+      <th scope="col">Price</th>
+      <th scope="col">Added on </th>
+      <th style="width: 10%" scope="col" class="sorttable_nosort">Buy</th>
+    </tr>
+</thead>
+</table>
 <script src="contollers/cpanelController.js"></script>
 </div>
 <?php require"footer.php"; ?>
