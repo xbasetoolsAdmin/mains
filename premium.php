26352f1235e4b64c200444bd287ade12d6e0ca37
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-26 16:08:10 +0100

    Update premium.php

--- premium.php
+++ premium.php
@@ -24,6 +24,7 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
  
             <div class="row m-2 pt-3" style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
                 <div class="col-sm-12 table-responsive">
+                        </div>
                    <table id="mainDiv" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);" ">
                
                 </table>
