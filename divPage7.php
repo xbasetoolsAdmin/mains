b0381a855791e44edacf3466b66aae1c08c20b78
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-27 02:54:50 +0100

    Update divPage7.php

--- divPage7.php
+++ divPage7.php
@@ -11,21 +11,71 @@ if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
 $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 ?>
 
-<table width=“100%”  class=“table table-striped table-bordered table-condensed sticky-header” id=“table”>
-<thead>
-    <tr>
-      <th scope=“col” >Country</th>
-      <th scope=“col”>TLD</th>
-      <th scope=“col”>Detect Hosting</th>
-      <th scope=“col”>Seller</th>
-      <th scope=“col”>Check</th>
-      <th scope=“col”>Price</th>
-      <th scope=“col”>Added on </th>
+<div class="row m-2 pt-3" style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
+<div class="col-sm-12 table-responsive" id="mainDiv">
+ <table id="table" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);" ">
+                        <thead>
+                            <tr>
+      <th data-priority="1">     </th>
+      <th class="all">    Country</th>
+      <th data-priority=“4”>Website</th>
+      <th data-priority=“3”>Details</th>
+      <th data-priority="5">Seller</th>
+   <!--<th data-priority=“7”>checker </th>-->
+      <th data-priority="5">Price   </th>
+      <th data-priority=“6”>Added on </th>
+               <th class=“all”>Buy</th>
+                         </tr>
+                     </thead>
+                 <tbody>
+<?php
+		include(“cr.php”);
+	    $q = mysqli_query($dbcon, “SELECT * FROM cpanels WHERE sold=‘0’ ORDER BY RAND()”)or die(mysql_error());
+	   	function srl($item)
+		{
+		$item0 = $item;
+		$item1 = rtrim($item0);
+		$item2 = ltrim($item1);
+		return $item2;
+		} 
 
-      <th scope=“col”>Buy</th>
-    </tr>
-</thead>
-  <tbody>
+ while($row = mysqli_fetch_assoc($q)){
+	 	 $countryfullname = $row[‘country’];
+	  $code = array_search(“$countryfullname”, $countrycodes);
+	 $countrycode = strtolower($code);
+
+	 $url = $row[‘url’];
+	 	$d = explode(“|”, $url);
+		$urled = srl($d[0]);
+
+	 	  $tld = end(explode(“.”, parse_url($urled, PHP_URL_HOST))); 
+    $qer = mysqli_query($dbcon, “SELECT * FROM resseller WHERE username=‘”.$row[‘resseller’].”’”)or die(mysql_error());
+		   while($rpw = mysqli_fetch_assoc($qer))
+			 $SellerNick = “seller”.$rpw[“id”].””;
+     echo “
+ <tr>    
+    <td id=‘cpanel_country’><i class=‘flag-icon flag-icon-$countrycode’></i>&nbsp;”.htmlspecialchars($row[‘country’]).” </td>
+		    <td id=‘cpanel_tld’> .”.$tld.” </td>
+    <td id=‘cpanel_hosting’> “.htmlspecialchars($row[‘infos’]).” </td>
+    <td id=‘cpanel_seller’> “.htmlspecialchars($SellerNick).”</td>
+“;
+	 echo ‘<td><span id=“shop’.$row[“id”].’” type=“cpanel”><a onclick=“javascript:check(‘.$row[“id”].’);” class=“btn btn-info btn-xs”><font color=white>Check</font></a></span><center></td>’;
+echo “
+    <td> “.htmlspecialchars($row[‘price’]).”</td>
+	    <td> “.htmlspecialchars($row[‘date’]).”</td>
+    “;
+
+    echo ‘
+    <td>
+	<span id=“cpanel’.$row[‘id’].’” title=“buy” type=“cpanel”><a onclick=“javascript:buythistool(‘.$row[‘id’].’)” class=“btn btn-primary btn-xs”><font color=white>Buy</font></a></span><center>
+    </td>
+            </tr>
+     ‘;
+ }
+
+ ?>
+
+
 <?php
 		include(“cr.php”);
 	    $q = mysqli_query($dbcon, “SELECT * FROM cpanels WHERE sold=‘0’ ORDER BY RAND()”)or die(mysql_error());
@@ -187,7 +237,7 @@ function openitem(order){
 	 	  $tld = end(explode(“.”, parse_url($urled, PHP_URL_HOST))); 
     $qer = mysqli_query($dbcon, “SELECT * FROM resseller WHERE username=‘”.$row[‘resseller’].”’”)or die(mysql_error());
 		   while($rpw = mysqli_fetch_assoc($qer))
-			 $SellerNick = “seller”.$rpw[“id”].””;
+			 $SellerNick = “seller”.$rpw[“id”].”no data availabile”;
      echo “
  <tr>    
     <td id=‘cpanel_country’><i class=‘flag-icon flag-icon-$countrycode’></i>&nbsp;”.htmlspecialchars($row[‘country’]).” </td>
