9515b7b42af4c550740a5fac4879eaac4ef232d8
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-27 15:42:13 +0100

    Update banks.php

--- banks.php
+++ banks.php
@@ -16,19 +16,20 @@ color:var(--font-color)
 .dataTables_wrapper .dataTables_paginate .paginate_button{color:var(--font-color)}#bank_data_paginate .paginate_button{color:var(--font-color)}.alert-info{color:var(--color-info);background-color:var(--color-backinfo);border-color:var(--color-borderinfo)}#banks_data_filter{color:var(--font-color)}#banks_data_length{color:var(--font-color)}#banks_data_paginate{color:var(--font-color)}#banks_data_info{color:var(--font-color)}</style>
 <ul class="nav nav-tabs">
     <div class="alert alert-info text-center" role="alert" style="margin: 15px;">
-        <ul>
-<h4>Static</h4>
-<ul class="user-info">
-<li>Number of Bank Accounts : <b><?php
-$s11 = mysqli_query($dbcon, "SELECT * FROM banks WHERE sold='0'");
-$r11=mysqli_num_rows($s11);;?></b></li>
-l
-        </ul>
+    
+    
     </div>
-l
+
+
+    <div class="alert alert-info text-center" role="alert" style="margin: 15px;">
+    
+    
+    
+
 <div class="row m-2 pt-3" style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
 <div class="col-sm-12 table-responsive" id="mainDiv">
 <table id="banks_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);" ">
+    
         <thead>
             <tr>
                  <th>ID </th>
