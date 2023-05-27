9fc03692b9cbb64ad71bf838bc364bb0e4a1cdd6
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-27 22:33:46 +0100

    Update banks.php

--- banks.php
+++ banks.php
@@ -16,50 +16,51 @@ color:var(--font-color)
 .dataTables_wrapper .dataTables_paginate .paginate_button{color:var(--font-color)}#bank_data_paginate .paginate_button{color:var(--font-color)}.alert-info{color:var(--color-info);background-color:var(--color-backinfo);border-color:var(--color-borderinfo)}#banks_data_filter{color:var(--font-color)}#banks_data_length{color:var(--font-color)}#banks_data_paginate{color:var(--font-color)}#banks_data_info{color:var(--font-color)}</style>
 <ul class="nav nav-tabs">
     <div class="alert alert-info text-center" role="alert" style="margin: 15px;">
-    
-    
-    </div>
+
+ `<!-- DataTables CSS -->
+     <link href="vendor/datatables-plugins/dataTables.bootstrap.css" 
+     rel="stylesheet">
+
+     <!-- DataTables Responsive CSS -->
+     <link href="vendor/datatables-responsive/dataTables.responsive.css" 
+     rel="stylesheet">
 
 
-    
-    
-<table id="example" class="display" style="width:100%">
+     <table class="table table-striped table-bordered table-hover" id="example">
         <thead>
             <tr>
-                <th>First name</th>
-                <th>Last name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Start date</th>
-                <th>Salary</th>
-            </tr>
-        </thead>
-        <tfoot>
-            <tr>
-                <th>First name</th>
-                <th>Last name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Start date</th>
-                <th>Salary</th>
-            </tr>
-        </tfoot>
-    </table>
-<script  src="controllers/0fgdgfddffkjhgdfxd.js">
+            <th>First Name</th>
+            <th>Surname</th>
+            <th>Gender</th>
+            <th>Birth Date</th>
+            <th>Address</th>
+            <th>Nationality</th>
+            <th>County</th>
+            <th>Student Type</th>
+            <th>Class</th>
+            <th>Operations</th>
+ // DONT USE COLSPAN WHILE USING DATATABLES
+            <th></th>
+            <th></th>
+        </tr>
+     </thead>
+     <tbody>
 
-</script>             
-<?php require"footer.php";?>
-<script  type="text/javascript">
-$(document).ready(function () {
-    $('#example').DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: 'data/objects.txt
-        ',
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+   <script src="vendor/datatables/js/jquery.dataTables.min.js"></script>
+    <script src="vendor/datatables-responsive/dataTables.responsive.js">
+    </script>
+
+
+    <script>
+    $(document).ready(function() {
+     $('#example').DataTable({
+         responsive: true
+     });
     });
-});
-</script>
-
-</div>
-</body>
-</html>
+    </script> `  
