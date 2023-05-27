9ac9ac7776a17e7c43b5d9bb6a79ecabf4f43b4e
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-27 13:26:02 +0100

    Update divPage8.php

--- divPage8.php
+++ divPage8.php
@@ -27,17 +27,39 @@
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>."id".</td>
-                <td>Name</td>
-                <td>Position</td>
-                <td>Tiger Nixon</td>
-                <td>System Architect</td>
-                <td>Edinburgh</td>
-                <td>61</td>
-                <td>2011-04-25</td>
-                <td>$320,800</td>
+
+ <?php
+include("cr.php");
+$q = mysqli_query($dbcon, "SELECT * FROM accounts WHERE sold='0' ORDER BY RAND()")or die(mysqli_error());
+ while($row = mysqli_fetch_assoc($q)){
+	 
+	 	 $countryfullname = $row['country'];
+	  $code = array_search("$countryfullname", $countrycodes);
+	 $countrycode = strtolower($code);
+	    $qer = mysqli_query($dbcon, "SELECT * FROM resseller WHERE username='".$row['resseller']."'")or die(mysql_error());
+		   while($rpw = mysqli_fetch_assoc($qer))
+			 $SellerNick = "seller".$rpw["id"]."";
+     echo "
+ <tr>      
+                  <td> </td>
+	  <td> ".$row['id']."</td>			       
+    <td id='bank_country'><i class='flag-icon flag-icon-$countrycode'></i>&nbsp;".htmlspecialchars($row['country'])." </td>
+    <td id='bank_sitename'> ".htmlspecialchars($row['bankname'])." </td> 
+    <td> ".htmlspecialchars($row['balance'])." </td> 
+	<td> ".htmlspecialchars($row['infos'])." </td>
+    <td id='bank_seller'> ".htmlspecialchars($SellerNick)."</td>
+    <td> ".htmlspecialchars($row['price'])."</td>
+	    <td> ".$row['date']."</td>";
+    echo '
+    <td>
+	<span id="bank'.$row['id'].'" title="buy" type="bank"><a onclick="javascript:buythistool('.$row['id'].')" class="btn btn-primary btn-xs"><font color=white>Buy</font></a></span><center>
+    </td>
             </tr>
+     ';
+ }
+
+	  ?>
+ 
     </tbody>
 </table>
 <script type="text/javascript">
