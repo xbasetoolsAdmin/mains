a0a11bc28cc05846f34e7171670cddc4bdfdb693
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-27 04:08:27 +0100

    Update premium.php

--- premium.php
+++ premium.php
@@ -60,18 +60,19 @@ $query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `accounts` WHER
     </div>
 
 
+
+
     <div class="row m-2 pt-3 " style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
-        <div class="col-sm-12 table-responsive" id="mainDiv">
+        <div class="col-sm-12 table-responsive">
             <table id="accounts_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);">
                 <thead>
                     <tr>
                         <th data-priority="1"></th>
                         <th class="all">ID</th>
                         <th data-priority="3">Country</th>
-                        <th data-priority="6">Description</th>
-                        <th data-priority="7">Email N</th>
-                        <th data-priority="8">Seller</th>
-                  
+                        <th data-priority="8">Website</th>
+                        <th data-priority="6">Details</th>
+                        <th data-priority="7">Seller</th>
                         <th data-priority="9">Price</th>
                         <th data-priority="10">Added on </th>
                         <th class="all">Buy</th>
