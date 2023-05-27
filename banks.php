a5adc3200b7af92d0bf16a7038a7d585d61e4987
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-27 15:50:43 +0100

    Update banks.php

--- banks.php
+++ banks.php
@@ -27,9 +27,9 @@ color:var(--font-color)
     
 
 <div class="row m-2 pt-3" style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
-<div class="col-sm-12 table-responsive" id="mainDiv">
+<div class="col-sm-12 table-responsive">
 
-<table id="banks_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);" ">
+<table id="banks_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);" id="mainDiv">
     
         <thead>
             <tr>
