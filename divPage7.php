128ab7025eb8c240778af622dbb519fae5b08f85
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-27 03:02:43 +0100

    Update divPage7.php

--- divPage7.php
+++ divPage7.php
@@ -27,9 +27,11 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
                          </tr>
                      </thead>
                  <tbody>
+</thead>
+  <tbody>
 <?php
-		include(“cr.php”);
-	    $q = mysqli_query($dbcon, “SELECT * FROM accounts WHERE sold=‘0’ ORDER BY RAND()”)or die(mysql_error());
+		include("cr.php");
+	    $q = mysqli_query($dbcon, "SELECT * FROM cpanels WHERE sold='0' ORDER BY RAND()")or die(mysql_error());
 	   	function srl($item)
 		{
 		$item0 = $item;
@@ -39,42 +41,40 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 		} 
 
  while($row = mysqli_fetch_assoc($q)){
-	 	 $countryfullname = $row[‘country’];
-	  $code = array_search(“$countryfullname”, $countrycodes);
+	 	 $countryfullname = $row['country'];
+	  $code = array_search("$countryfullname", $countrycodes);
 	 $countrycode = strtolower($code);
 
-	 $url = $row[‘url’];
-	 	$d = explode(“|”, $url);
+	 $url = $row['url'];
+	 	$d = explode("|", $url);
 		$urled = srl($d[0]);
 
-	 	  $tld = end(explode(“.”, parse_url($urled, PHP_URL_HOST))); 
-    $qer = mysqli_query($dbcon, “SELECT * FROM resseller WHERE username=‘”.$row[‘resseller’].”’”)or die(mysql_error());
+	 	  $tld = end(explode(".", parse_url($urled, PHP_URL_HOST))); 
+    $qer = mysqli_query($dbcon, "SELECT * FROM resseller WHERE username='".$row['resseller']."'")or die(mysql_error());
 		   while($rpw = mysqli_fetch_assoc($qer))
-			 $SellerNick = “seller”.$rpw[“id”].””;
-     echo “
+			 $SellerNick = "seller".$rpw["id"]."";
+     echo "
  <tr>    
-    <td id=‘cpanel_country’><i class=‘flag-icon flag-icon-$countrycode’></i>&nbsp;”.htmlspecialchars($row[‘country’]).” </td>
-		    <td id=‘cpanel_tld’> .”.$tld.” </td>
-    <td id=‘cpanel_hosting’> “.htmlspecialchars($row[‘infos’]).” </td>
-    <td id=‘cpanel_seller’> “.htmlspecialchars($SellerNick).”</td>
-“;
-	 echo ‘<td><span id=“shop’.$row[“id”].’” type=“cpanel”><a onclick=“javascript:check(‘.$row[“id”].’);” class=“btn btn-info btn-xs”><font color=white>Check</font></a></span><center></td>’;
-echo “
-    <td> “.htmlspecialchars($row[‘price’]).”</td>
-	    <td> “.htmlspecialchars($row[‘date’]).”</td>
-    “;
+    <td id='cpanel_country'><i class='flag-icon flag-icon-$countrycode'></i>&nbsp;".htmlspecialchars($row['country'])." </td>
+		    <td id='cpanel_tld'> .".$tld." </td>
+    <td id='cpanel_hosting'> ".htmlspecialchars($row['infos'])." </td>
+    <td id='cpanel_seller'> ".htmlspecialchars($SellerNick)."</td>
+";
+	 echo '<td><span id="shop'.$row["id"].'" type="cpanel"><a onclick="javascript:check('.$row["id"].');" class="btn btn-info btn-xs"><font color=white>Check</font></a></span><center></td>';
+echo "
+    <td> ".htmlspecialchars($row['price'])."</td>
+	    <td> ".htmlspecialchars($row['date'])."</td>
+    ";
 
-    echo ‘
+    echo '
     <td>
-	<span id=“cpanel’.$row[‘id’].’” title=“buy” type=“cpanel”><a onclick=“javascript:buythistool(‘.$row[‘id’].’)” class=“btn btn-primary btn-xs”><font color=white>Buy</font></a></span><center>
+	<span id="cpanel'.$row['id'].'" title="buy" type="cpanel"><a onclick="javascript:buythistool('.$row['id'].')" class="btn btn-primary btn-xs"><font color=white>Buy</font></a></span><center>
     </td>
             </tr>
-     ‘;
+     ';
  }
 
  ?>
-
-
 <?php
 		include(“cr.php”);
 	    $q = mysqli_query($dbcon, “SELECT * FROM cpanels WHERE sold=‘0’ ORDER BY RAND()”)or die(mysql_error());
@@ -196,139 +196,3 @@ function openitem(order){
 </div>
 
 
-<div class="row m-2 pt-3" style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
-<div class="col-sm-12 table-responsive" id="mainDiv">
- <table id="table" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);" ">
-                        <thead>
-                            <tr>
-      <th data-priority="1">     </th>
-      <th class="all">    Country</th>
-      <th data-priority=“4”>Website</th>
-      <th data-priority=“3”>Details</th>
-      <th data-priority="5">Seller</th>
-   <!--<th data-priority=“7”>checker </th>-->
-      <th data-priority="5">Price   </th>
-      <th data-priority=“6”>Added on </th>
-               <th class=“all”>Buy</th>
-                         </tr>
-                     </thead>
-                 <tbody>
-<?php
-		include(“cr.php”);
-	    $q = mysqli_query($dbcon, “SELECT * FROM cpanels WHERE sold=‘0’ ORDER BY RAND()”)or die(mysql_error());
-	   	function srl($item)
-		{
-		$item0 = $item;
-		$item1 = rtrim($item0);
-		$item2 = ltrim($item1);
-		return $item2;
-		} 
-
- while($row = mysqli_fetch_assoc($q)){
-	 	 $countryfullname = $row[‘country’];
-	  $code = array_search(“$countryfullname”, $countrycodes);
-	 $countrycode = strtolower($code);
-
-	 $url = $row[‘url’];
-	 	$d = explode(“|”, $url);
-		$urled = srl($d[0]);
-
-	 	  $tld = end(explode(“.”, parse_url($urled, PHP_URL_HOST))); 
-    $qer = mysqli_query($dbcon, “SELECT * FROM resseller WHERE username=‘”.$row[‘resseller’].”’”)or die(mysql_error());
-		   while($rpw = mysqli_fetch_assoc($qer))
-			 $SellerNick = “seller”.$rpw[“id”].”no data availabile”;
-     echo “
- <tr>    
-    <td id=‘cpanel_country’><i class=‘flag-icon flag-icon-$countrycode’></i>&nbsp;”.htmlspecialchars($row[‘country’]).” </td>
-		    <td id=‘cpanel_tld’> .”.$tld.” </td>
-    <td id=‘cpanel_hosting’> “.htmlspecialchars($row[‘infos’]).” </td>
-    <td id=‘cpanel_seller’> “.htmlspecialchars($SellerNick).”</td>
-“;
-	 echo ‘<td><span id=“shop’.$row[“id”].’” type=“cpanel”><a onclick=“javascript:check(‘.$row[“id”].’);” class=“btn btn-info btn-xs”><font color=white>Check</font></a></span><center></td>’;
-echo “
-    <td> “.htmlspecialchars($row[‘price’]).”</td>
-	    <td> “.htmlspecialchars($row[‘date’]).”</td>
-    “;
-
-    echo ‘
-    <td>
-	<span id=“cpanel’.$row[‘id’].’” title=“buy” type=“cpanel”><a onclick=“javascript:buythistool(‘.$row[‘id’].’)” class=“btn btn-primary btn-xs”><font color=white>Buy</font></a></span><center>
-    </td>
-            </tr>
-     ‘;
- }
-
- ?>
-
- </tbody>
- </table>
-
-<script type=“text/javascript”>
-$(‘#filterbutton’).click(function () {$(“#table tbody tr”).each(function() {var ck1 = $.trim( $(this).find(“#cpanel_country”).text().toLowerCase() );var ck2 = $.trim( $(this).find(“#cpanel_tld”).text().toLowerCase() );var ck3 = $.trim( $(this).find(“#cpanel_hosting”).text().toLowerCase() );var ck4 = $.trim( $(this).find(“#cpanel_seller”).text().toLowerCase() ); var val1 = $.trim( $(‘select[name=“cpanel_country”]’).val().toLowerCase() );var val2 = $.trim( $(‘input[name=“cpanel_tld”]’).val().toLowerCase() );var val3 = $.trim( $(‘input[name=“cpanel_hosting”]’).val().toLowerCase() );var val4 = $.trim( $(‘select[name=“cpanel_seller”]’).val().toLowerCase() ); if((ck1 != val1 && val1 != ‘’ ) || ck2.indexOf(val2)==-1 || ck3.indexOf(val3)==-1 || (ck4 != val4 && val4 != ‘’ )){ $(this).hide();  }else{ $(this).show(); } });$(‘#filterbutton’).prop(‘disabled’, true);});$(‘.filterselect’).change(function () {$(‘#filterbutton’).prop(‘disabled’, false);});$(‘.filterinput’).keyup(function () {$(‘#filterbutton’).prop(‘disabled’, false);});
-function buythistool(id){
-  bootbox.confirm(“Are you sure?”, function(result) {
-        if(result ==true){
-      $.ajax({
-     method:”GET”,
-     url:”buytool.php?id=“+id+”&t=cpanels”,
-     dataType:”text”,
-     success:function(data){
-         if(data.match(/<button/)){
-		 $(“#cpanel”+id).html(data).show();
-         }else{
-            bootbox.alert(‘<center><img src=“files/img/balance.png”><h2><b>No enough balance !</b></h2><h4>Please refill your balance <a class=“btn btn-primary btn-xs”  href=“addBalance.html” onclick=“window.open(this.href);return false;” >Add Balance <span class=“glyphicon glyphicon-plus”></span></a></h4></center>’)
-         }
-     },
-   });
-       ;}
-  });
-}
-g:xcheck=0;
-function check(id){   
-     if(xcheck > 1){
-    bootbox.alert(“<b>Wait</b> - Other checking operation is executed!”);
-  } else {
-    xcheck++;
-    var type = $(“#shop”+id).attr(‘type’)
-	$(“#shop”+id).html(‘Checking...’).show();
-	$.ajax({
-	type: 		‘GET’,
-	url: 		‘CheckCpanel’+id+’.html’,
-	success:	function(data)
-	{
-		$(“#shop”+id).html(data).show();
-		xcheck—;
-	}});
-} }
-
-function openitem(order){
-  $(“#myModalLabel”).text(‘Order #’+order);
-  $(‘#myModal’).modal(‘show’);
-  $.ajax({
-    type:       ‘GET’,
-    url:        ‘showOrder’+order+’.html’,
-    success:    function(data)
-    {
-        $(“#modelbody”).html(data).show();
-    }});
-
-}
-
-</script>
-<div class=“modal fade” id=“myModal” tabindex=“-1” role=“dialog” aria-labelledby=“myModalLabel”>
-  <div class=“modal-dialog” role=“document”>
-    <div class=“modal-content”>
-      <div class=“modal-header”>
-        <button type=“button” class=“close” data-dismiss=“modal” aria-label=“Close”><span aria-hidden=“true”>&times;</span></button>
-        <h4 class=“modal-title” id=“myModalLabel”></h4>
-      </div>
-      <div class=“modal-body” id=“modelbody”>
-
-
-      </div>
-      <div class=“modal-footer”>
-        <button type=“button” class=“btn btn-primary” data-dismiss=“modal”>Close</button>
-      </div>
-    </div>
-  </div>
-</div>
