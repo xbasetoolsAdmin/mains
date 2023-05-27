970ec8043c3b2bf5cda4da1c047aabf100a54d2d
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-27 13:43:01 +0100

    Update divPage8.php

--- divPage8.php
+++ divPage8.php
@@ -16,9 +16,11 @@
 
 <table id="banks_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);" ">
         <thead>
+
             <tr>
-                <th>   </th>
-                 <th> ID </th>
+                 <th> </th>
+                 <th>ID </th>
+                <th>Country</th>
                 <th>Name</th>
                 <th>Position</th>
                 <th>Office</th>
