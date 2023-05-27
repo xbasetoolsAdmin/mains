b3a1b73091e55ef9650dd93e7e533fdf127b6525
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-28 00:26:24 +0100

    Update banks.php

--- banks.php
+++ banks.php
@@ -39,20 +39,28 @@ $uid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 								
 								
 								
-		   <div class="alert alert-info text-left" role="alert" style="margin: 15px;">
+		   <div class="alert alert-info text-center" role="alert" style="margin: 15px;">
 <ul class="nav nav-tabs">
 		<li class="active"><a href="#static" data-toggle="tab" aria-expanded="true">Static</a></li>
-		<li class=""><a href="#all" data-toggle="tab" aria-expanded="false" onclick="TabDiv('all','banksTab1.php')">All</a></li>
-		<li class=""><a href="#add" data-toggle="tab" aria-expanded="false" onclick="TabDiv('add','banksTab2.php')">Add</a></li>
 		
-	<!--	<li class=""><a href="#mass" data-toggle="tab" aria-expanded="false" onclick="TabDiv('mass','banksTab3.php')">Mass Add</a></li> -->
-	</ul>
-	
+		
+		
+		
+		
+		<li class=""><a href="#all" data-toggle="tab" aria-expanded="false" onclick="TabDiv('all','banksTab1.php')">All</a></li>
+		
+		
+		
 <div id="myTabContent" class="tab-content">
     <div class="tab-pane fade active in" id="static"> 
+				
+				
 <div class="well well-sm">
 <h4>Rules</h4>
+
 <ul class="user-info">
+	
+	
 <li><b>Do not insert a bank account without SCREENSHOT of it (USE : prntscr.com)</b></li>
 <li><b>ONLY INSERT WORKING ACCOUNT</b></li>
 <li>If you have mistaken or need to edit a tool just remove it and add it again</li>
