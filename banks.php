fd825c9f03d285989d922990cbdbdd5443a0dd21
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-28 00:40:33 +0100

    Update banks.php

--- banks.php
+++ banks.php
@@ -26,8 +26,8 @@ $uid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
             <li>We Have Fresh  Banks Logs With Fullz info + E-mails access  of  Diffrents Countries ( You Can Use it for Spam ). </li>
             <li>Our E-mails Lists Guarante for you Good results because most of them are from Shops and Big Sites .</li>
             <li>Combo use for cracking (Brute forcing attack) not use for login to email.</li>
-        <li>There is <b> <b><?php $s12 = mysqli_query($dbcon, "SELECT * FROM banks where resseller='$uid'");$r11=mysqli_num_rows($s12);
- echo $r11;?></b> </b> Available.</li>
+       <center>				<h5><li>There is <b> <b><?php $s12 = mysqli_query($dbcon, "SELECT * FROM banks where resseller='$uid'");$r11=mysqli_num_rows($s12);
+ echo $r11;?></b> </b> Available.</li></h5><center>
         </ul>
     </div>
       </div>
