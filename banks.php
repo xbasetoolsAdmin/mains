87cd7c7a89182694d75c1bc849cf128d5c0a2dd1
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-27 15:18:50 +0100

    Update banks.php

--- banks.php
+++ banks.php
@@ -19,8 +19,13 @@ color:var(--font-color)
         <ul>
 <h4>Static</h4>
 <ul class="user-info">
-<li>Number of Bank Accounts : <b><?php $s12 = mysqli_query($dbcon, "SELECT * FROM banks where resseller='$uid'");$r11=mysqli_num_rows($s12);
+<li>Number of Bank Accounts : <b><?php
+$s11 = mysqli_query($dbcon, "SELECT * FROM banks WHERE sold='0'");
+
+$r11=mysqli_num_rows($s10);
  echo $r11;?></b></li>
+
+$s11 = mysqli_query($dbcon, "SELECT * FROM banks WHERE sold='0'");
         </ul>
     </div>
     <input type=hidden id="type" name="type" value="8" />
