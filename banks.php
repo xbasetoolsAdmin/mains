753b15595d8ca65436442ddc43d14c8dc34630e6
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-28 08:21:42 +0100

    Update banks.php

--- banks.php
+++ banks.php
@@ -211,6 +211,50 @@
 </table>
 <!--end::Datatable-->
 <script>
+
+$(document).ready(function(){
+    $('#dataList').DataTable({
+        "processing": true,
+        "serverSide": true,
+        "ajax": "data/objects.php",
+
+        "columnDefs": [
+            { "orderable": false, "targets": 7 }
+        ]
+    });
+});    
+</script>
+<script>
+$(document).ready(function () {
+    // Setup - add a text input to each footer cell
+    $('#example tfoot th').each(function () {
+        var title = $(this).text();
+        $(this).html('<input type="text" placeholder="Search ' + title + '" />');
+    });
+ 
+    // DataTable
+    var table = $('#example').DataTable({
+        initComplete: function () {
+            // Apply the search
+            this.api()
+                .columns()
+                .every(function () {
+                    var that = this;
+ 
+                    $('input', this.footer()).on('keyup change clear', function () {
+                        if (that.search() !== this.value) {
+                            that.search(this.value).draw();
+                        }
+                    });
+                });
+        },
+    });
+});
+</scrpt>
+
+
+<script>
+
 $(document).ready(function(){
     $('#dataList').DataTable({
         "processing": true,
