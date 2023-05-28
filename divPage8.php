aec56991ca676692f7b6dfd5e138b0ff65da9226
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-28 06:52:46 +0100

    Update divPage8.php

--- divPage8.php
+++ divPage8.php
@@ -1,72 +1,70 @@
 <?php
-ob_start();
-session_start();
-date_default_timezone_set('UTC');
-if (!isset($_SESSION['sname']) and !isset($_SESSION['spass']))
-{
-				header("location: ../");
-				exit();
-} //!isset($_SESSION['sname']) and !isset($_SESSION['spass'])
-$usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
-$uid   = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
-
-<?php 
  
-// Database connection info 
-$dbDetails = array( 
-    'host' => 'localhost', 
-    'user' => 'xbaseleets_members', 
-    'pass' => 'xbaseleets_members', 
-    'db'   => 'xbaseleets_members' 
-); 
+/*
+ * DataTables example server-side processing script.
+ *
+ * Please note that this script is intentionally extremely simple to show how
+ * server-side processing can be implemented, and probably shouldn't be used as
+ * the basis for a large complex system. It is suitable for simple use cases as
+ * for learning.
+ *
+ * See http://datatables.net/usage/server-side for full details on the server-
+ * side processing requirements of DataTables.
+ *
+ * @license MIT - http://datatables.net/license_mit
+ */
  
-// DB table to use 
-$table = 'members'; 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Easy set variables
+ */
  
-// Table's primary key 
-$primaryKey = 'id'; 
+// DB table to use
+$table = 'datatables_demo';
  
-// Array of database columns which should be read and sent back to DataTables. 
-// The `db` parameter represents the column name in the database.  
-// The `dt` parameter represents the DataTables column identifier. 
-$columns = array( 
-    array( 'db' => 'first_name', 'dt' => 0 ), 
-    array( 'db' => 'last_name',  'dt' => 1 ), 
-    array( 'db' => 'email',      'dt' => 2 ), 
-    array( 'db' => 'gender',     'dt' => 3 ), 
-    array( 'db' => 'country',    'dt' => 4 ), 
-    array( 
-        'db'        => 'created', 
-        'dt'        => 5, 
-        'formatter' => function( $d, $row ) { 
-            return date( 'jS M Y', strtotime($d)); 
-        } 
-    ), 
-    array( 
-        'db'        => 'status', 
-        'dt'        => 6, 
-        'formatter' => function( $d, $row ) { 
-            return ($d == 1)?'Active':'Inactive'; 
-        } 
-    ), 
-    array( 
-        'db'        => 'id', 
-        'dt'        => 7, 
-        'formatter' => function( $d, $row ) { 
-            return ' 
-                <a href="edit.php?id='.$d.'">Edit</a>&nbsp; 
-                <a href="delete.php?id='.$d.'">Delete</a> 
-            '; 
-        } 
-    ) 
-); 
+// Table's primary key
+$primaryKey = 'id';
  
-// Include SQL query processing class 
-require 'ssp.class.php'; 
+// Array of database columns which should be read and sent back to DataTables.
+// The `db` parameter represents the column name in the database, while the `dt`
+// parameter represents the DataTables column identifier. In this case object
+// parameter names
+$columns = array(
+    array( 'db' => 'first_name', 'dt' => 'first_name' ),
+    array( 'db' => 'last_name',  'dt' => 'last_name' ),
+    array( 'db' => 'email',   'dt' => 'email' ),
+    array( 'db' => 'gender',     'dt' => 'gender' ),
+    array(
+        'db'        => 'country',
+        'dt'        => 'created_date',
+        'formatter' => function( $d, $row ) {
+            return date( 'jS M y', strtotime($d));
+        }
+    ),
+    array(
+        'db'        => 'email',
+        'dt'        => 'email',
+        'formatter' => function( $d, $row ) {
+            return '$'.number_format($d);
+        }
+    )
+);
  
-// Output data as json format 
-echo json_encode( 
-    SSP::simple( $_GET, $dbDetails, $table, $primaryKey, $columns ) 
-); 
+// SQL server connection information
+$sql_details = array(
+    'user' => 'xbaseleets_members',
+    'pass' => 'xbaseleets_members',
+    'db'   => 'xbaseleets_members',
+    'host' => ''
+);
  
-?>
+ 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * If you just want to use the basic configuration for DataTables with PHP
+ * server-side, there is no need to edit below this line.
+ */
+ 
+require( 'ssp.class.php' );
+ 
+echo json_encode(
+    SSP::simple( $_POST, $sql_details, $table, $primaryKey, $columns )
+);
