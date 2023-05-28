8666bdcb3c4e9b5b09fc1159af67e3e6ab84608b
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-28 16:06:59 -0700

    Update leads.php

--- leads.php
+++ leads.php
@@ -419,57 +419,12 @@
 				</span>
 			</label>
 		</div
-		});
-	});
-	function setTooltip(btn, message) {
-	//console.log("hide-1");
-	$(btn).tooltip('hide')
-	.attr('data-original-title', message)
-	.tooltip('show');
-	//console.log("show");
-	}
-	function hideTooltip(btn) {
-	setTimeout(function() {$(btn).tooltip('hide'); /*console.log("hide-2");*/}, 1000);
-	}
-	</script>
-]
+	
 	<div style="padding-left:20px; display:inline-block; margin-bottom:10px">
 		<span><a href="addBalance.php?do=balance" class="btn btn-primary" style="color:#fff; font-size: 12 px">Add Balance</a></span>
 		<span><a href="addBalance.php?do=details" class="btn btn-primary" style="color:#fff; font-size: 12 px">Balance Info</a></span>
 	</div>
 	<div id="mainDiv"></div>
-	<script type="text/javascript">
-	$(document).keydown(function(event){
-	if(event.which=="17")
-	cntrlIsPressed = true;
-	});
-	$(document).keyup(function(){
-	cntrlIsPressed = false;
-	});
-	var cntrlIsPressed = false;
-	$(window).on("popstate", function(e) {
-	location.replace(document.location);
-	});
-	$(window).on('load', function() {
-			$('.dropdown').hover(function(){ $('.dropdown-toggle', this).trigger('click'); });
-			pageDiv(1,'Add Balance - OdinShop','rdp',1);
-		var clipboard = new Clipboard('.copyit');
-		clipboard.on('success', function(e) {
-		setTooltip(e.trigger, 'Copied!');
-		hideTooltip(e.trigger);
-		e.clearSelection();
-		});
-	});
-	function setTooltip(btn, message) {
-	//console.log("hide-1");
-	$(btn).tooltip('hide')
-	.attr('data-original-title', message)
-	.tooltip('show');
-	//console.log("show");
-	}
-	function hideTooltip(btn) {
-	setTimeout(function() {$(btn).tooltip('hide'); /*console.log("hide-2");*/}, 1000);
-	}
-	</script>
-</body>
-</html>
+	<script type="text/javascript" src="5dfyuiilfkjnfbxvxcgvh.js">	
+		</body>
+		</html>
