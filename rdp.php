8c4d20b7ecc5703fa2642b7cf6f98b5c9138bf39
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-28 14:30:04 -0700

    Update rdp.php

--- rdp.php
+++ rdp.php
@@ -14,24 +14,64 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
             
  </ul>
 </div>
-<style>.display td{background:var(--color-card);color:var(--font-color)}.dataTables_wrapper .dataTables_paginate .paginate_button{color:var(--font-color)}#accounts_data_paginate .paginate_button{color:var(--font-color)}.alert-info{color:var(--color-info);background-color:var(--color-backinfo);border-color:var(--color-borderinfo)}#accounts_data_filter{color:var(--font-color)}#accounts_data_length{color:var(--font-color)}#accounts_data_paginate{color:var(--font-color)}#accounts_data_info{color:var(--font-color)}
+<style>
+            .display td{background:var(--color-card);color:var(--font-color)}.dataTables_wrapper .dataTables_paginate .paginate_button{color:var(--font-color)}#accounts_data_paginate .paginate_button{color:var(--font-color)}.alert-info{color:var(--color-info);background-color:var(--color-backinfo);border-color:var(--color-borderinfo)}#accounts_data_filter{color:var(--font-color)}#accounts_data_length{color:var(--font-color)}#accounts_data_paginate{color:var(--font-color)}#accounts_data_info{color:var(--font-color)}
 </style>
 <div style="padding-center:20px; display:inline-block; margin-bottom:10px">
 
 </div>
+<div style="padding-left:20px; display:inline-block; margin-bottom:10px">
+<span><a href="addBalance.php?do=balance" class="btn btn-primary" style="color:#fff; font-size: 12 px">Add Balance</a></span>
+<span><a href="addBalance.php?do=details" class="btn btn-primary" style="color:#fff; font-size: 12 px">Balance Info</a></span>
+</div>
+<div id="mainDiv"></div>
+<script type="text/javascript">
 
- 
- 
-            <div class="row m-2 pt-3" style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
-                <div class="col-sm-12 table-responsive">
-                        </div>
-                   <table id="mainDiv" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);" ">
-               
-                </table>
-                </div>
-          </div>
-     </div>
+    $(document).keydown(function(event){
+        if(event.which=="17")
+            cntrlIsPressed = true;
+    });
 
+    $(document).keyup(function(){
+        cntrlIsPressed = false;
+    });
+
+    var cntrlIsPressed = false;
+
+
+    $(window).on("popstate", function(e) {
+        location.replace(document.location);
+
+    });
+
+
+    $(window).on('load', function() {
+		$('.dropdown').hover(function(){ $('.dropdown-toggle', this).trigger('click'); });
+	   	pageDiv(13,'Add Balance - OdinShop','addBalance',1);
+	   var clipboard = new Clipboard('.copyit');
+	    clipboard.on('success', function(e) {
+	      setTooltip(e.trigger, 'Copied!');
+	      hideTooltip(e.trigger);
+	      e.clearSelection();
+	   });
+
+});
+
+
+function setTooltip(btn, message) {
+  //console.log("hide-1");
+  $(btn).tooltip('hide')
+    .attr('data-original-title', message)
+    .tooltip('show');
+     //console.log("show");
+}
+
+function hideTooltip(btn) {
+  setTimeout(function() {$(btn).tooltip('hide'); /*console.log("hide-2");*/}, 1000);
+}
+</script>
+</body>
+</html>
  <?php require"footer.php";?>
 //<script type="text/javascript">
 ///$(document).keydown((function(event){"17"==event.which&&(cntrlIsPressed=!0)})),$(document).keyup((function(){cntrlIsPressed=!1}));var cntrlIsPressed=!1;function setTooltip(btn,message){$(btn).tooltip("hide").attr("data-original-title",message).tooltip("show")}function hideTooltip(btn){setTimeout((function(){$(btn).tooltip("hide")}),1e3)}$(window).on("popstate",(function(e){location.replace(document.location)})),$(window).on("load",(function(){$(".dropdown").hover((function(){$(".dropdown-toggle",this).trigger("click")})),rdpData(1,"RDPs - xBaseTools","rdp",1),new Clipboard(".copyit").on("success",(function(e){setTooltip(e.trigger,"Copied!"),hideTooltip(e.trigger),e.clearSelection()}))}));
