66877cd7be5bd52755f210997209076e4d382373
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-28 08:55:08 +0100

    Update banks.php

--- banks.php
+++ banks.php
@@ -66,13 +66,6 @@
         }
     </style>
 </head>
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
-<!-- DataTables CSS and JS library -->
-<link rel="stylesheet" type="text/css" href="DataTables/datatables.min.css"/>
-<script type="text/javascript" src="DataTables/datatables.min.js"></script>
-
 	<h2>Banks</h2>
         
         
@@ -144,63 +137,9 @@
 		</div>		
     
     </div>		
-                   <div id="page-content-wrapper">
-            <div class="container-fluid">
-      <div id="divPage">
-<table id="dataList" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);">
-<!--begin::Wrapper-->
-<div class="d-flex flex-stack mb-5">
-    <!--begin::Search-->
-    <div class="d-flex align-items-center position-relative my-1">
-        <i class="ki-duotone ki-magnifier fs-1 position-absolute ms-6"><span class="path1"></span><span class="path2"></span></i>
-        <input type="text" data-kt-docs-table-filter="search" class="form-control form-control-solid w-250px ps-15" placeholder="Search Customers"/>
-    </div>
-    <!--end::Search-->
-
-    <!--begin::Toolbar-->
-    <div class="d-flex justify-content-end" data-kt-docs-table-toolbar="base">
-        <!--begin::Filter-->
-        <button type="button" class="btn btn-light-primary me-3" data-bs-toggle="tooltip" title="Coming Soon">
-            <i class="ki-duotone ki-filter fs-2"><span class="path1"></span><span class="path2"></span></i>
-            Filter
-        </button>
-        <!--end::Filter-->
-
-        <!--begin::Add customer-->
-        <button type="button" class="btn btn-primary" data-bs-toggle="tooltip" title="Coming Soon">
-            <i class="ki-duotone ki-plus fs-2"></i>
-            Add Customer
-        </button>
-        <!--end::Add customer-->
-    </div>
-    <!--end::Toolbar-->
-
-    <!--begin::Group actions-->
-    <div class="d-flex justify-content-end align-items-center d-none" data-kt-docs-table-toolbar="selected">
-        <div class="fw-bold me-5">
-            <span class="me-2" data-kt-docs-table-select="selected_count"></span> Selected
-        </div>
-
-        <button type="button" class="btn btn-danger" data-bs-toggle="tooltip" title="Coming Soon">
-            Selection Action
-        </button>
-    </div>
-    <!--end::Group actions-->
-</div>
-<!--end::Wrapper-->
-
-<!--begin::Datatable-->
-<table id="kt_datatable_example_1" class="table align-middle table-row-dashed fs-6 gy-5">
-    <thead>
-    <tr class="text-start text-gray-400 fw-bold fs-7 text-uppercase gs-0">
-        <th class="w-10px pe-2">
-            <div class="form-check form-check-sm form-check-custom form-check-solid me-3">
-                <input class="form-check-input" type="checkbox" data-kt-check="true" data-kt-check-target="#kt_datatable_example_1 .form-check-input" value="1"/>
-            </div>
-        </th>
-        <th>Customer Name</th>                                                                                                                             <table id="example" class="display" style="width:100%">
+      <table id="dataList" class="display" style="width:100%">
         <thead>
-            <tr>
+            <tr>tr>
                 <th>Name</th>
                 <th>Position</th>
                 <th>Office</th>
@@ -241,13 +180,13 @@ $(document).ready(function(){
 <script>
 $(document).ready(function () {
     // Setup - add a text input to each footer cell
-    $('#example tfoot th').each(function () {
+    $('#dataList tfoot th').each(function () {
         var title = $(this).text();
         $(this).html('<input type="text" placeholder="Search ' + title + '" />');
     });
  
     // DataTable
-    var table = $('#example').DataTable({
+    var table = $('#dataList').DataTable({
         initComplete: function () {
             // Apply the search
             this.api()
