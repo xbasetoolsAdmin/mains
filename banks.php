0671bfe125812483abb3d8a5af71bf854f5200e7
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-28 01:22:47 +0100

    Update banks.php

--- banks.php
+++ banks.php
@@ -11,45 +11,7 @@
        <center>				<h5><li>There is <b> <b><?php $s12 = mysqli_query($dbcon, "SELECT * FROM banks where resseller='$uid'");$r11=mysqli_num_rows($s12);
  echo $r11;?></b> </b> Available.</li></h5><center>
         </ul>
-      
-      
-      
-      
-      
-                                    </div>
-                               </div>
-                         </div> 
-                     </div>
-                </div>       
-            </div>
-          </div>
-     </div>
- </div>
-<table width="100%" id="dataTable" class="display table-responsive table-hover dataTable no-footer" role="" aria-describedby="dataTable_info" style="width: 100%;">        
-                                      <thead>
-                                        <tr> 
-                                     <th>ID</th>
-                                   <th>Type</th>
-                                  <th>Country</th>
-                               <th>Bank Name</th>
-                                <th>Balance</th>
-                              <th>Information</th>
-                                <th>Seller</th>
-                                 <th>Date added</th>
-                                  <th>Price</th>
-                                  <th>Action</th>
-                                      </tr>
-                                   </thead>
-                            <tbody id='tbody2'>
-                                 </table>
-                               </div>
-                         </div> 
-                     </div>
-                </div>       
-            </div>
-          </div>
-     </div>
- </div>
+       </div>
 
 
 
