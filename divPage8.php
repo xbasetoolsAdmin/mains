675abc26d1b381d176e5e865b76a645ecf2de9aa
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-28 05:12:00 -0700

    Update divPage8.php

--- divPage8.php
+++ divPage8.php
@@ -1,70 +1,64 @@
 <?php
- 
-/*
- * DataTables example server-side processing script.
- *
- * Please note that this script is intentionally extremely simple to show how
- * server-side processing can be implemented, and probably shouldn't be used as
- * the basis for a large complex system. It is suitable for simple use cases as
- * for learning.
- *
- * See http://datatables.net/usage/server-side for full details on the server-
- * side processing requirements of DataTables.
- *
- * @license MIT - http://datatables.net/license_mit
- */
- 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * Easy set variables
- */
- 
-// DB table to use
-$table = 'datatables_demo';
- 
-// Table's primary key
-$primaryKey = 'id';
- 
-// Array of database columns which should be read and sent back to DataTables.
-// The `db` parameter represents the column name in the database, while the `dt`
-// parameter represents the DataTables column identifier. In this case object
-// parameter names
-$columns = array(
-    array( 'db' => 'first_name', 'dt' => 'first_name' ),
-    array( 'db' => 'last_name',  'dt' => 'last_name' ),
-    array( 'db' => 'email',   'dt' => 'email' ),
-    array( 'db' => 'gender',     'dt' => 'gender' ),
-    array(
-        'db'        => 'country',
-        'dt'        => 'created_date',
-        'formatter' => function( $d, $row ) {
-            return date( 'jS M y', strtotime($d));
-        }
-    ),
-    array(
-        'db'        => 'email',
-        'dt'        => 'email',
-        'formatter' => function( $d, $row ) {
-            return '$'.number_format($d);
-        }
-    )
+include 'includes/config.php';
+
+## Read value
+$draw = $_POST['draw'];
+$row = $_POST['start'];
+$rowperpage = $_POST['length']; // Rows display per page
+$columnIndex = $_POST['order'][0]['column']; // Column index
+$columnName = $_POST['columns'][$columnIndex]['data']; // Column name
+$columnSortOrder = $_POST['order'][0]['dir']; // asc or desc
+$searchValue = $_POST['search']['value']; // Search value
+
+## Custom Field value
+$searchByName = $_POST['searchByName'];
+$searchByGender = $_POST['searchByGender'];
+
+## Search 
+$searchQuery = " ";
+if($searchByName != ''){
+    $searchQuery .= " and (emp_name like '%".$searchByName."%' ) ";
+}
+if($searchByGender != ''){
+    $searchQuery .= " and (gender='".$searchByGender."') ";
+}
+if($searchValue != ''){
+	$searchQuery .= " and (emp_name like '%".$searchValue."%' or 
+        email like '%".$searchValue."%' or 
+        city like'%".$searchValue."%' ) ";
+}
+
+## Total number of records without filtering
+$sel = mysqli_query($con,"select count(*) as allcount from employee");
+$records = mysqli_fetch_assoc($sel);
+$totalRecords = $records['allcount'];
+
+## Total number of records with filtering
+$sel = mysqli_query($con,"select count(*) as allcount from employee WHERE 1 ".$searchQuery);
+$records = mysqli_fetch_assoc($sel);
+$totalRecordwithFilter = $records['allcount'];
+
+## Fetch records
+$empQuery = "select * from employee WHERE 1 ".$searchQuery." order by ".$columnName." ".$columnSortOrder." limit ".$row.",".$rowperpage;
+$empRecords = mysqli_query($con, $empQuery);
+$data = array();
+
+while ($row = mysqli_fetch_assoc($empRecords)) {
+    $data[] = array(
+    		"emp_name"=>$row['emp_name'],
+    		"email"=>$row['email'],
+    		"gender"=>$row['gender'],
+    		"salary"=>$row['salary'],
+    		"city"=>$row['city']
+    	);
+}
+
+## Response
+$response = array(
+    "draw" => intval($draw),
+    "iTotalRecords" => $totalRecords,
+    "iTotalDisplayRecords" => $totalRecordwithFilter,
+    "aaData" => $data
 );
- 
-// SQL server connection information
-$sql_details = array(
-    'user' => 'xbaseleets_members',
-    'pass' => 'xbaseleets_members',
-    'db'   => 'xbaseleets_members',
-    'host' => ''
-);
- 
- 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * If you just want to use the basic configuration for DataTables with PHP
- * server-side, there is no need to edit below this line.
- */
- 
-require( 'ssp.class.php' );
- 
-echo json_encode(
-    SSP::simple( $_POST, $sql_details, $table, $primaryKey, $columns )
-);
+
+echo json_encode($response);
