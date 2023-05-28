3dfa3b298e1af8f5a7fdc14d63cf45f9fd9be3c7
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-28 15:31:36 -0700

    Update rdp.php

--- rdp.php
+++ rdp.php
@@ -11,10 +11,15 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
 ]
 
 
+
+
+<div style="padding-left:20px; display:inline-block; margin-bottom:10px">
+<span><a href="addBalance.php?do=balance" class="btn btn-primary" style="color:#fff; font-size: 12 px">Add Balance</a></span>
+<span><a href="addBalance.php?do=details" class="btn btn-primary" style="color:#fff; font-size: 12 px">Balance Info</a></span>
+</div>
 <div id="mainDiv"></div>
-<script type="text/javascript" src="function_cuntructors/8xfcglkjfsfdfghjjklk.js">
-</script>
 <script type="text/javascript">
+
     $(document).keydown(function(event){
         if(event.which=="17")
             cntrlIsPressed = true;
@@ -34,8 +39,8 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
 
 
     $(window).on('load', function() {
-    $('.dropdown').hover(function(){ $('.dropdown-toggle', this).trigger('click'); });
-    rdpData(1,'Add Balance - OdinShop','rdp',1);
+		$('.dropdown').hover(function(){ $('.dropdown-toggle', this).trigger('click'); });
+	 	pageDiv(1,'rdp - xBaseTools','rdp',1);
 	   var clipboard = new Clipboard('.copyit');
 	    clipboard.on('success', function(e) {
 	      setTooltip(e.trigger, 'Copied!');
@@ -58,11 +63,5 @@ function hideTooltip(btn) {
   setTimeout(function() {$(btn).tooltip('hide'); /*console.log("hide-2");*/}, 1000);
 }
 </script>
-</body>
-</html>
- <?php require"footer.php";?>
-//<script type="text/javascript">
-///$(document).keydown((function(event){"17"==event.which&&(cntrlIsPressed=!0)})),$(document).keyup((function(){cntrlIsPressed=!1}));var cntrlIsPressed=!1;function setTooltip(btn,message){$(btn).tooltip("hide").attr("data-original-title",message).tooltip("show")}function hideTooltip(btn){setTimeout((function(){$(btn).tooltip("hide")}),1e3)}$(window).on("popstate",(function(e){location.replace(document.location)})),$(window).on("load",(function(){$(".dropdown").hover((function(){$(".dropdown-toggle",this).trigger("click")})),rdpData(1,"RDPs - xBaseTools","rdp",1),new Clipboard(".copyit").on("success",(function(e){setTooltip(e.trigger,"Copied!"),hideTooltip(e.trigger),e.clearSelection()}))}));
- </script>
 </body>
 </html>
