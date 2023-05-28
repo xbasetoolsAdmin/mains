1972bc5e8f25160b7395b0d86b140ecde0c5f5d0
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-28 02:07:17 +0100

    Update banks.php

--- banks.php
+++ banks.php
@@ -15,7 +15,59 @@ if($r['resseller'] != "1"){
 
 
 
+	<h2>Banks</h2>
+<?php
 
+date_default_timezone_set('UTC');
+
+if(!isset($_SESSION['sname']) and !isset($_SESSION['spass'])){
+   header("location: ../");
+   exit();
+}
+$usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
+$uid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
+?> 
+        
+		
+<ul class="nav nav-tabs">
+		<li class="active"><a href="#static" data-toggle="tab" aria-expanded="true">Static</a></li>
+		<li class=""><a href="#all" data-toggle="tab" aria-expanded="false" onclick="TabDiv('all','banksTab1.php')">All</a></li>
+		<li class=""><a href="#add" data-toggle="tab" aria-expanded="false" onclick="TabDiv('add','banksTab2.php')">Add</a></li>
+	<!--	<li class=""><a href="#mass" data-toggle="tab" aria-expanded="false" onclick="TabDiv('mass','banksTab3.php')">Mass Add</a></li> -->
+	</ul>
+<div id="myTabContent" class="tab-content">
+    <div class="tab-pane fade active in" id="static"> 
+<div class="well well-sm">
+<h4>Rules</h4>
+<ul class="user-info">
+<li><b>Do not insert a bank account without SCREENSHOT of it (USE : prntscr.com)</b></li>
+<li><b>ONLY INSERT WORKING ACCOUNT</b></li>
+<li>If you have mistaken or need to edit a tool just remove it and add it again</li>
+<li><b>Deleted</b> mean that the tools is not working !</li>
+</ul>
+<h4>Static</h4>
+<ul class="user-info">
+<li>Number of Bank Accounts : <b><?php $s12 = mysqli_query($dbcon, "SELECT * FROM banks where resseller='$uid'");$r11=mysqli_num_rows($s12);
+ echo $r11;?></b></li>
+<li>Unsold Bank Accounts : <b><?php $s12 = mysqli_query($dbcon, "SELECT * FROM banks where resseller='$uid' and sold='0'");$r11=mysqli_num_rows($s12);
+ echo $r11;?></b></li>
+<li>Sold Bank Accounts : <b><?php $s12 = mysqli_query($dbcon, "SELECT * FROM banks where resseller='$uid' and sold='1'");$r11=mysqli_num_rows($s12);
+ echo $r11;?></b></li>
+<li>Deleted Bank Accounts : <b><?php $s12 = mysqli_query($dbcon, "SELECT * FROM banks where resseller='$uid' and sold='deleted'");$r11=mysqli_num_rows($s12);
+ echo $r11;?></b></li>
+</ul>
+      </div>
+      </div>
+	  
+	<div class="tab-pane fade" id="all"></div>
+    <div class="tab-pane fade" id="add"></div>
+
+</div>
+</div></div>
+            </div>
+            </div>
+        </div>
+        
 
 <form id="banksAdd">
 <div class="row">
@@ -301,59 +353,7 @@ if($r['resseller'] != "1"){
 <div class="row">
 	<div class="well well-sm col-md-6"><b>[Response]</b><div id="result"></div></div>
 </div>
-	<h2>Banks</h2>
-<?php
 
-date_default_timezone_set('UTC');
-
-if(!isset($_SESSION['sname']) and !isset($_SESSION['spass'])){
-   header("location: ../");
-   exit();
-}
-$usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
-$uid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
-?> 
-        
-		
-<ul class="nav nav-tabs">
-		<li class="active"><a href="#static" data-toggle="tab" aria-expanded="true">Static</a></li>
-		<li class=""><a href="#all" data-toggle="tab" aria-expanded="false" onclick="TabDiv('all','banksTab1.php')">All</a></li>
-		<li class=""><a href="#add" data-toggle="tab" aria-expanded="false" onclick="TabDiv('add','banksTab2.php')">Add</a></li>
-	<!--	<li class=""><a href="#mass" data-toggle="tab" aria-expanded="false" onclick="TabDiv('mass','banksTab3.php')">Mass Add</a></li> -->
-	</ul>
-<div id="myTabContent" class="tab-content">
-    <div class="tab-pane fade active in" id="static"> 
-<div class="well well-sm">
-<h4>Rules</h4>
-<ul class="user-info">
-<li><b>Do not insert a bank account without SCREENSHOT of it (USE : prntscr.com)</b></li>
-<li><b>ONLY INSERT WORKING ACCOUNT</b></li>
-<li>If you have mistaken or need to edit a tool just remove it and add it again</li>
-<li><b>Deleted</b> mean that the tools is not working !</li>
-</ul>
-<h4>Static</h4>
-<ul class="user-info">
-<li>Number of Bank Accounts : <b><?php $s12 = mysqli_query($dbcon, "SELECT * FROM banks where resseller='$uid'");$r11=mysqli_num_rows($s12);
- echo $r11;?></b></li>
-<li>Unsold Bank Accounts : <b><?php $s12 = mysqli_query($dbcon, "SELECT * FROM banks where resseller='$uid' and sold='0'");$r11=mysqli_num_rows($s12);
- echo $r11;?></b></li>
-<li>Sold Bank Accounts : <b><?php $s12 = mysqli_query($dbcon, "SELECT * FROM banks where resseller='$uid' and sold='1'");$r11=mysqli_num_rows($s12);
- echo $r11;?></b></li>
-<li>Deleted Bank Accounts : <b><?php $s12 = mysqli_query($dbcon, "SELECT * FROM banks where resseller='$uid' and sold='deleted'");$r11=mysqli_num_rows($s12);
- echo $r11;?></b></li>
-</ul>
-      </div>
-      </div>
-	  
-	<div class="tab-pane fade" id="all"></div>
-    <div class="tab-pane fade" id="add"></div>
-
-</div>
-</div></div>
-            </div>
-            </div>
-        </div>
-        
         
         
         
