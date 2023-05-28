bac4e95192f4de7ecb517bbacaad9142108d53f3
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-28 06:37:39 +0100

    Update divPage8.php

--- divPage8.php
+++ divPage8.php
@@ -2,7 +2,6 @@
 ob_start();
 session_start();
 date_default_timezone_set('UTC');
-include "includes/config.php";
 if (!isset($_SESSION['sname']) and !isset($_SESSION['spass']))
 {
 				header("location: ../");
@@ -16,9 +15,9 @@ $uid   = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 // Database connection info 
 $dbDetails = array( 
     'host' => 'localhost', 
-    'user' => 'root', 
-    'pass' => 'root', 
-    'db'   => 'codexworld' 
+    'user' => 'xbaseleets_members', 
+    'pass' => 'xbaseleets_members', 
+    'db'   => 'xbaseleets_members' 
 ); 
  
 // DB table to use 
