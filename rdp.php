888f68bb287403efd18211d306224fdaa193d509
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-29 07:09:50 -0700

    Update rdp.php

--- rdp.php
+++ rdp.php
@@ -14,10 +14,35 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
 
 
 <div style="padding-left:20px; display:inline-block; margin-bottom:10px">
-<span><a href="addBalance.php?do=balance" class="btn btn-primary" style="color:#fff; font-size: 12 px">Add Balance</a></span>
-<span><a href="addBalance.php?do=details" class="btn btn-primary" style="color:#fff; font-size: 12 px">Balance Info</a></span>
+<table id="table" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);" "="">
 </div>
-<div id="mainDiv"></div>
+					<thead>
+						<tr>
+							<th data-priority="1"></th>
+							<th class="all">ID</th>
+							<th data-priority="3">Country</th>
+							<th data-priority="8">Type</th>
+							<th data-priority="15">TLD</th>
+							<th data-priority="14">Domain</th>
+							<th data-priority="11">Hosting</th>
+							<th data-priority="12">CMS</th>
+							<th class="all" style="width:9% !important;">Ip Blacklist</th>
+							<th class="all">Seo Info</th>
+							<th data-priority="9">Source</th>
+							<th class="all">Technologies</th>
+							<th data-priority="10">Seller</th>
+							<th class="all">Check</th>
+							<th class="all">Price</th>
+							<th data-priority="13">Added on
+							</th>
+							<th class="all">Send Test</th>
+							<th data-priority="4">DA</th>
+							<th data-priority="5">PA</th>
+							<th class="all">Buy</th>
+						</tr>
+					</thead>
+
+								       
 <script type="text/javascript">
 
     $(document).keydown(function(event){
