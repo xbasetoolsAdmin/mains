f37dfc469af956bb9a0607d684cbf35989a58816
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-29 23:12:20 +0100

    Update objects.php

--- data/objects.php
+++ data/objects.php
@@ -1,56 +1,42 @@
-{
-  "data": [
-    {
-      "0": "1",
-      "1": "5421",
-      "2": "Tiger Nixon",
-      "3": "System Architect",
-      "4": "$320,800",
-      "5": "2011/04/25",
-      "6": "Edinburgh",      
-      "7": "Edinburgh",
-      "8": "Edinburgh",
-      "9": "Edinburgh"
-      
-    },
-    {
-
-      "0": "1",
-      "1": "5421",
-      "2": "Tiger Nixon",
-      "3": "System Architect",
-      "4": "$320,800",
-      "5": "2011/04/25",
-      "6": "Edinburgh",      
-      "7": "Edinburgh",
-      "8": "Edinburgh",
-      "9": "Edinburgh"
-    },
-    {
-
-      "0": "1",
-      "1": "5421",
-      "2": "Tiger Nixon",
-      "3": "System Architect",
-      "4": "$320,800",
-      "5": "2011/04/25",
-      "6": "Edinburgh",      
-      "7": "Edinburgh",
-      "8": "Edinburgh",
-      "9": "Edinburgh"
-    }
-    {
-
-      "0": "1",
-      "1": "5421",
-      "2": "Tiger Nixon",
-      "3": "System Architect",
-      "4": "$320,800",
-      "5": "2011/04/25",
-      "6": "Edinburgh",      
-      "7": "Edinburgh",
-      "8": "Edinburgh",
-      "9": "Edinburgh"
-    },
-  ]
-}
+[
+  {
+    "ID": ".$row['id'].",
+    "Country": "&nbsp;\".htmlspecialchars($row['country']).\" ",
+    "Site Name": ".htmlspecialchars($row['sitename']).",
+    "Available Information": ".htmlspecialchars($row['infos']).",
+    "Seller": ".htmlspecialchars($SellerNick).",
+    "Price": ".htmlspecialchars($row['price']).",
+    "Added on ": ".$row['date'].",
+    "Buy": "\n\tBuy\n    "
+  },
+  {
+    "ID": ".$row['id'].",
+    "Country": "&nbsp;\".htmlspecialchars($row['country']).\" ",
+    "Site Name": ".htmlspecialchars($row['sitename']).",
+    "Available Information": ".htmlspecialchars($row['infos']).",
+    "Seller": ".htmlspecialchars($SellerNick).",
+    "Price": ".htmlspecialchars($row['price']).",
+    "Added on ": ".$row['date'].",
+    "Buy": "\n\tBuy\n    "
+  },
+  {
+    "ID": "ID",
+    "Country": "Country",
+    "Site Name": "Site Name",
+    "Available Information": "Available Information",
+    "Seller": "Seller",
+    "Price": "Price",
+    "Added on ": "Added on ",
+    "Buy": "Buy"
+  },
+  {
+    "ID": "ID",
+    "Country": "Country",
+    "Site Name": "Site Name",
+    "Available Information": "Available Information",
+    "Seller": "Seller",
+    "Price": "Price",
+    "Added on ": "Added on ",
+    "Buy": "Buy"
+  }
+]
