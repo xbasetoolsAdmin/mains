2c0152a6fe8c67a2e59b5721f6e82a20314ec6d3
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-29 07:16:57 -0700

    Update main.php

--- main.php
+++ main.php
@@ -1,8 +1,7 @@
 <?php
 ob_start();
 session_start();
-include "header.php";
-date_default_timezone_set('UTC');
+ date_default_timezone_set('UTC');
 include "includes/config.php";
 if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
     header("location: ./");
