5de44c8b3f1da507985f0d9aca3b561b4df6bff4
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-29 07:53:40 -0700

    Update divPage1.php

--- divPage1.php
+++ divPage1.php
@@ -10,14 +10,10 @@ if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
 }
 $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 ?>
-
-
-
-
    <div class="row m-2 pt-3 " style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
-                <div class="col-sm-12 table-responsive">
-                    <table id="rdp_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);">
-                        <thead>
+<div class="col-sm-12 table-responsive">
+<table id="rdp_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);">
+             <thead>
 				<tr>
 				                	<th data-priority="1"></th>
 							<th class="all">ID</th>
@@ -30,7 +26,7 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 					<th class="all" style="width:9% !important;">Ip Blacklist</th>
 							<th class="all">Buy</th>
 						</tr>
-			 </thead>
+	  	  </thead>
 	    <tbody>
 		    <?php
 include("cr.php");
@@ -43,28 +39,31 @@ $q = mysqli_query($dbcon,"SELECT * FROM rdps WHERE sold='0' ORDER BY RAND()")or 
 		 $qer = mysqli_query($dbcon, "SELECT * FROM resseller WHERE username='".$row['resseller']."'")or die(mysql_error());
 		   while($rpw = mysqli_fetch_assoc($qer))
 			 $SellerNick = "seller".$rpw["id"]."";
-     echo "
- <tr>    
+                                        
+		                                   echo " <tr>    
+     
+     
+    <th data-priority='1'>                                                                                           </th> 
+    <th data-priority='1'>                                                                                           </th>
     <td id='rdp_country'><i class='flag-icon flag-icon-$countrycode'></i>&nbsp;".htmlspecialchars($row['country'])." </td>
-    <td> ".htmlspecialchars($row['city'])." </td>
-    <td id='rdp_windows'> ".htmlspecialchars($row['windows'])." </td>
-    <td> ".htmlspecialchars($row['ram'])." </td>
-    <td id='rdp_access'> ".htmlspecialchars($row['access'])." </td>
-    <td id='rdp_hosting'> ".htmlspecialchars($row['hosting'])."</td>
-    <td id='rdp_seller'> ".htmlspecialchars($SellerNick)."</td>
-    <td> ".htmlspecialchars($row['price'])."</td>
-	    <td> ".htmlspecialchars($row['date'])."</td>
-    <td>";
-    echo '
-	<span id="rdp'.$row['id'].'" title="buy" type="cpanel"><a onclick="javascript:buythistool('.$row['id'].')" class="btn btn-primary btn-xs"><font color=white>Buy</font></a></span><center>
-    </td>
-            </tr>
-     ';
+    <td>				 ".htmlspecialchars($row['city'])."					    </td>
+    <td id='rdp_windows'>		 ".htmlspecialchars($row['windows'])."					    </td>
+    <td>				 ".htmlspecialchars($row['ram'])." 					    </td>
+    <td id='rdp_access'>		 ".htmlspecialchars($row['access'])."					    </td>
+    <td id='rdp_hosting'>		 ".htmlspecialchars($row['hosting'])."			   		    </td>
+    <td id='rdp_seller'>		 ".htmlspecialchars($SellerNick).			    		   "</td>
+    <td> " 				.htmlspecialchars($row['price']).    			     	           "</td>
+    <td> "				.htmlspecialchars($row['date']). 					  " </td>
+    <td>";                                         
+		                                          echo '
+    <span id="rdp'				.$row['id'].'  			            " title="buy" type="cpanel">
+    <a onclick="                   javascript:buythistool('.$row['id'].')              " class="btn btn-primary btn-xs">
+    <font color=white>					Buy				     </font></a> </span><center>
+                                                                                                                     </td>
+                                                       </tr> ';
  }
 
- ?>
-			    
-			    
+ ?> 	    
   <tfoot>
 			    <tr>
                                                 	<th data-priority="1"></th>
