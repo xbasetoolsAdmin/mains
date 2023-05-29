c6bbb3bc07dd9f56d5bb249d70e3ba873d5eb5f5
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-29 07:20:46 -0700

    Update divPage8.php

--- divPage8.php
+++ divPage8.php
@@ -1,1 +1,32 @@
 
+
+
+
+
+<div style="padding-left:20px; display:inline-block; margin-bottom:10px">
+<div class="divtable" id="mainDiv" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);" "="">
+<thead>
+						<tr>
+							<th data-priority="1"></th>
+							<th class="all">ID</th>
+							<th data-priority="3">Country</th>
+							<th data-priority="8">Type</th>
+							<th data-priority="15">TLD</th>
+							<th data-priority="14">Domain</th>
+							<th data-priority="11">Hosting</th>
+							<th data-priority="12">CMS</th>
+							<th class="all" style="width:9% !important;">Ip Blacklist</th>
+							<th class="all">Seo Info</th>
+							<th data-priority="9">Source</th>
+							<th class="all">Technologies</th>
+							<th data-priority="10">Seller</th>
+							<th class="all">Check</th>
+							<th class="all">Price</th>
+							<th data-priority="13">Added on
+							</th>
+							<th class="all">Send Test</th>
+							<th data-priority="4">DA</th>
+							<th data-priority="5">PA</th>
+							<th class="all">Buy</th>
+						</tr>
+					</thead>
