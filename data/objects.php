81e4efb11313dd4357e0abc86da4e7d508e58a6e
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-30 03:57:49 +0100

    Update objects.php

--- data/objects.php
+++ data/objects.php
@@ -73,45 +73,4 @@ echo json_encode(
 
 
 
-[
-  {
-    "ID": ".$row['id'].",
-    "Country": "&nbsp;\".htmlspecialchars($row['country']).\" ",
-    "Site Name": ".htmlspecialchars($row['sitename']).",
-    "Available Information": ".htmlspecialchars($row['infos']).",
-    "Seller": ".htmlspecialchars($SellerNick).",
-    "Price": ".htmlspecialchars($row['price']).",
-    "Added on ": ".$row['date'].",
-    "Buy": "\n\tBuy\n    "
-  },
-  {
-    "ID": ".$row['id'].",
-    "Country": "&nbsp;\".htmlspecialchars($row['country']).\" ",
-    "Site Name": ".htmlspecialchars($row['sitename']).",
-    "Available Information": ".htmlspecialchars($row['infos']).",
-    "Seller": ".htmlspecialchars($SellerNick).",
-    "Price": ".htmlspecialchars($row['price']).",
-    "Added on ": ".$row['date'].",
-    "Buy": "\n\tBuy\n    "
-  },
-  {
-    "ID": "ID",
-    "Country": "Country",
-    "Site Name": "Site Name",
-    "Available Information": "Available Information",
-    "Seller": "Seller",
-    "Price": "Price",
-    "Added on ": "Added on ",
-    "Buy": "Buy"
-  },
-  {
-    "ID": "ID",
-    "Country": "Country",
-    "Site Name": "Site Name",
-    "Available Information": "Available Information",
-    "Seller": "Seller",
-    "Price": "Price",
-    "Added on ": "Added on ",
-    "Buy": "Buy"
-  }
 ]
