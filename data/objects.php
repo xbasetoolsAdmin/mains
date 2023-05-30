7fc98ddb35f4ba35db24f8862fc0f51a0e99a67c
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-30 03:57:18 +0100

    Update objects.php

--- data/objects.php
+++ data/objects.php
@@ -1,3 +1,78 @@
+
+<?php
+
+/*
+ * DataTables example server-side processing script.
+ *
+ * Please note that this script is intentionally extremely simple to show how
+ * server-side processing can be implemented, and probably shouldn’t be used as
+ * the basis for a large complex system. It is suitable for simple use cases as
+ * for learning.
+ *
+ * See http://datatables.net/usage/server-side for full details on the server-
+ * side processing requirements of DataTables.
+ *
+ * @license MIT - http://datatables.net/license_mit
+ */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Easy set variables
+ */
+
+// DB table to use
+$table = ‘accounts’;
+
+// Table’s primary key
+$primaryKey = ‘id’;
+
+// Array of database columns which should be read and sent back to DataTables.
+// The `db` parameter represents the column name in the database, while the `dt`
+// parameter represents the DataTables column identifier. In this case simple
+// indexes
+$columns = array(
+	array( ‘db’ => ‘country’, ‘dt’ => 0 ),
+	array( ‘db’ => ‘sitename’,  ‘dt’ => 1 ),
+	array( ‘db’ => ‘infos’,   ‘dt’ => 2 ),
+	array( ‘db’ => ‘price’,     ‘dt’ => 3 ),
+	array(
+		‘db’        => ‘start_date’,
+		‘dt’        => 4,
+		‘formatter’ => function( $d, $row ) {
+			return date( ‘jS M y’, strtotime($d));
+		}
+	),
+	array(
+		‘db’        => ‘salary’,
+		‘dt’        => 5,
+		‘formatter’ => function( $d, $row ) {
+			return ‘$’.number_format($d);
+		}
+	)
+);
+
+// SQL server connection information
+$sql_details = array(
+	‘user’ => ‘xbaseleets_xbaseleets’,
+	‘pass’ => ‘xbaseleets_xbaseleets',
+	‘db’   => ‘xbaseleets_xbaseleets',
+	‘host’ => ‘localhost'
+);
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * If you just want to use the basic configuration for DataTables with PHP
+ * server-side, there is no need to edit below this line.
+ */
+
+require( ‘../cr.php’ );
+
+echo json_encode(
+	SSP::simple( $_GET, $sql_details, $table, $primaryKey, $columns )
+);
+
+
+
+
 [
   {
     "ID": ".$row['id'].",
