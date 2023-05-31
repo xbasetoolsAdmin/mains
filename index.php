26f1ab1bd61bf05739f9201a2c76e4baaf6891bf
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-31 15:37:52 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 
 
-<div class="alert alert-info text-center" role="alert" style="margin: 25px;">
+
+ <div class="alert alert-info text-center" role="alert" style="margin: 25px;">
         <ul>
             <li>We Have Fresh E-mails Lists for Diffrents Countries ( You Can Use it for Spam ). </li>
             <li>Our E-mails Lists Guarante for you Good results because most of them are from Shops and Big Sites .</li>
             <li>Combo use for cracking (Brute forcing attack) not use for login to email.</li>
             <li>There is <b> 0 </b> Available.</li>
         </ul>
-    </div>
+</div></center>
 
 
    <!---------------- input search ---------------->
