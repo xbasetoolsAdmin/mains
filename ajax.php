162ae00ae7ddca2cf0e7d12352001a87f355e4d9
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-31 13:51:54 +0100

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -18,10 +18,42 @@ $myObj =new stdClass();
 $myObj->tickets = "$r1";
 $myObj->tickets = "$r1";
 
+        $q = mysqli_query($dbcon, "SELECT resseller FROM users WHERE username='$usrid'");
+          $r = mysqli_fetch_assoc($q);
+		  $reselerif = $r['resseller'];
+       if ($reselerif == "1") { 
+          $q = mysqli_query($dbcon, "SELECT soldb FROM resseller WHERE username='$usrid'");
+          $r = mysqli_fetch_assoc($q);
+$seller = $r['soldb'];
+$myObj->seller = "$$seller";		
+	   } else { }
+$myJSON = json_encode($myObj);
+
+echo $myJSON;
+?>
+
+<?php
+ob_start();
+session_start();
+date_default_timezone_set('UTC');
+include "includes/config.php";
+
+if(!isset($_SESSION['sname']) and !isset($_SESSION['spass'])){
+   header("location: ../");
+   exit();
+}
+$usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
+
+$s1 = mysqli_query($dbcon, "SELECT * FROM ticket where status='1' and uid='$usrid'");
+$r1=mysqli_num_rows($s1);
+
+
+
 $myObj =new stdClass();
 $myObj->tickets = "$r1";
 $myObj->tickets = "$r1";
 $myObj->tickets = "$r1";
+
 $myObj->tickets = "$r1";
 
         $q = mysqli_query($dbcon, "SELECT resseller FROM users WHERE username='$usrid'");
