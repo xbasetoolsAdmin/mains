8d7a8f79d9686724f3259994ba379cff2cb643eb
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-31 05:24:12 +0100

    Update header.php

--- header.php
+++ header.php
@@ -9,14 +9,6 @@ exit();
 }
 $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 ?>
- <!DOCTYPE html>
-<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Main - home</title>
     
     <!DOCTYPE html>
 <html>
