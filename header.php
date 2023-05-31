d2839b9e459575d9f645ae40129360935587cdb5
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-31 03:50:04 +0100

    Update header.php

--- header.php
+++ header.php
@@ -27,10 +27,9 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 <script type="text/javascript" src="../buyer/js/jquery.datatables.min.js"></script>
 <font face="Arial">
 <link rel="stylesheet" href="../buyer/layout/css/bootstrap.min.css">
-		
-<style>
+     <style>
 
-.alert.alert-shadowed {
+.alert.alert-info {
     -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.2);
     -moz-box-shadow: 0 1px 2px rgba(0,0,0,.2);
     box-shadow: 0 1px 2px rgba(0,0,0,.2);
@@ -40,7 +39,7 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 .alert {
     margin-bottom: 20px;
     margin-top: 0;
-    color: #fff;
+    color: var(--font-color);
     border-width: 0;
     border-left-width: 5px;
     padding: 10px;
@@ -49,7 +48,7 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 
 .alert.alert-danger {
     border-color: #df5138;
-    background: #001f3f;
+    background-color: #2196f3;
 }
 
 .teddy-text {
@@ -69,22 +68,153 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 </style>
 <style>
 .content {
-	display:none;
+	display:td :
+    th;
 }
 
-</style>
-    <style>
-.sort {
-  .sortable
-}
-.sort th:not(.sorttable_sorted):not(.sorttable_sorted_reverse):not(.sorttable_nosort):after { 
-    content: " \25BE" 
-}
 </style>
 </head>
+<body class="them">
+<style>
+    .navbar-nav .dropdown-menu {
+        margin: 0 !important
+    }
+ 
+    .theme-light {
+        --color-primary: #0060df;
+        --color-secondary: #ffffff;
+        --color-secondary2: #ecf0f1;
+        --color-accent: #fd6f53;
+        --font-color: #000000;
+        --color-nav: #ffffff;
+        --color-dropdown: #ffffff;
+        --color-card: #ffffff;
+        --color-card2: #d1ecf1;
+        --color-info: #0c5460;
+        --color-backinfo: #d1ecf1;
+        --color-borderinfo: #bee5eb;
+ 
+    }
+ 
+    .theme-dark {
+        --color-primary: #17ed90;
+        --color-secondary: #353B50;
+        --color-secondary2: #353B50;
+        --color-accent: #12cdea;
+        --font-color: #ffffff;
+        --color-nav: #363947;
+        --color-dropdown: rgba(171, 205, 239, 0.3);
+        --color-card: #262A37;
+        --color-card2: #262A37;
+        --color-info: #4DD0E1;
+        --color-backinfo: #262A37;
+        --color-borderinfo: #262A37;
+    }
+ 
+    .them {
+ 
+        background: var(--color-secondary);
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+ 
+    .them h1 {
+        color: var(--font-color);
+        font-family: sans-serif;
+    }
+ 
+    .card-body {
+        color: var(--font-color);
+    }
+ 
+    .them button {
+        color: var(--font-color);
+        background-color: #ffffff;
+        padding: 10px 20px;
+        border: 0;
+        border-radius: 5px;
+    }
+ 
+    .navbar.navbar-light .navbar-toggler {
+        color: var(--font-color);
+    }
+ 
+    /* The switch - the box around the slider */
+    .switch {
+        position: relative;
+        display: inline-block;
+        width: 60px;
+        height: 34px;
+    }
+ 
+    /* Hide default HTML checkbox */
+    .switch input {
+        opacity: 0;
+        width: 0;
+        height: 0;
+    }
+ 
+    /* The slider */
+    .slider {
+        position: absolute;
+        cursor: pointer;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: #ccc;
+        -webkit-transition: 0.4s;
+        transition: 0.4s;
+    }
+ 
+    .slider:before {
+        position: absolute;
+        content: "";
+        height: 40px;
+        width: 40px;
+        left: 0px;
+        bottom: 4px;
+        top: 0;
+        bottom: 0;
+        margin: auto 0;
+        -webkit-transition: 0.4s;
+        transition: 0.4s;
+        box-shadow: 0 0px 15px #2020203d;
+        background: white url('https://i.ibb.co/FxzBYR9/night.png');
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+ 
+    input:checked+.slider {
+        background-color: #2196f3;
+    }
+ 
+    input:focus+.slider {
+        box-shadow: 0 0 1px #2196f3;
+    }
+ 
+    input:checked+.slider:before {
+        -webkit-transform: translateX(24px);
+        -ms-transform: translateX(24px);
+        transform: translateX(24px);
+        background: white url('https://i.ibb.co/7JfqXxB/sunny.png');
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+ 
+    /* Rounded sliders */
+    .slider.round {
+        border-radius: 34px;
+    }
+ 
+    .slider.round:before {
+        border-radius: 50%;
+    }
+</style>
+    
 
-<body>
-<nav class="navbar navbar-default navbar-fixed-top ">
+<nav class="navbar nabber-default navbar-fixed-top ">
             <div class="container-fluid">
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#topFixedNavbar1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
