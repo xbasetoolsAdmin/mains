473bf4739397fb59763c5c35c1a7fd4b0f887ae0
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-31 12:51:54 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,173 +1,132 @@
-<!DOCTYPE html>
-<html>
-<meta charset="UTF-8" />
-  
-<head>
-    <!-- jQuery -->
-    <script type="text/javascript" 
-    src="https://code.jquery.com/jquery-3.5.1.js">
-    </script>
-  
-    <!-- DataTables CSS -->
-    <link rel="stylesheet" href=
-"https://cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css">
-  
-    <!-- DataTables JS -->
-    <script src=
-"https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js">
-    </script>
-  
-    <h1 style="color: green;">GeeksForGeeks</h1>
-    <h3>DataTables processing Option</h3>
-<link rel="stylesheet" href="../buyer/layout/css/all.min.css" />
-<link rel="stylesheet" href="../buyer/layout/css/main.css" />
-<link rel="stylesheet" href="../buyer/layout/css/util.css" />
-<style>
-    body {
-        padding-top: 80px
-    }
-</style>
-<link rel="stylesheet" href="../buyer/layout/css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="../buyer/layout/css/flags.css" />
-<link href="//cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
-<link rel="stylesheet" href="../buyer/layout/fonts/iconic/css/material-design-iconic-font.min.css">
-<script src="../buyer/layout/js/main.js">
-</script>
-
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="alfacoins-site-verification" content=" ">
-    <meta name="revisit-after" content="2 days">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>xbaseTools</title>
-    <script type="text/javascript">
-        // Notice how this gets configured before we load Font Awesome
-        window.FontAwesomeConfig = {
-            autoReplaceSvg: false
-        }
-    </script>
-    <style>
-        @import url(//fonts.googleapis.com/css?family=Roboto:400);
-        .navbar-nav .dropdown-menu {
-            margin: 0 !important
-        }
-    </style>
-</head>
-<!-- body class -->
-
-<body class="them">
+ <div class="alert alert-info text-left" role="alert" style="margin: 15px;">
+        <ul>
+            <li>We Have Fresh E-mails Lists for Diffrents Countries ( You Can Use it for Spam ). </li>
+            <li>Our E-mails Lists Guarante for you Good results because most of them are from Shops and Big Sites .</li>
+            <li>Combo use for cracking (Brute forcing attack) not use for login to email.</li>
+            <li>There is <b> 0 </b> Available.</li>
+        </ul>
+    </div>
 
 
-    <style>
-    </style>
-
-    <nav class="navbar navbar-expand-xl navbar  navbar-light " style="
-
-                                                            position:fixed;
-
-                                                            background-color: var(--color-nav);
-
-                                                            z-index:1;
-
-                                                            top:0;
-
-                                                            left:0;
-
-                                                            right:0;
-
-                                                            line-height: 1.5;
-
-                                                            font-family: 'Lato', sans-serif;
-
-                                                            font-size: 15px;
-
-                                                            padding-top: 0.5rem;
-
-                                                            padding-right: 1rem;
-
-                                                            padding-bottom: 0.5rem;
-
-                                                          padding-left: 1rem;
-
-                                                        ">
-
-        <a class="navbar-brand" href="main" style="color: var(--font-color);"><img width="40px" src="layout/images/logo.png"> Odin</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <i class="navbar-toggler-icon"></i>
-        </button>
-        <div class="collapse navbar-collapse order-1" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-
-
-
-
-
-
-
-
-
-
-            </ul>
+   <!---------------- input search ---------------->
+    <input type=hidden id="type" name="type" value="5" />
+    <div class="row m-3 pt-1" style="color: var(--font-color);">
+        <div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
+          
+            <label for="infos" style="margin-bottom: 10px; margin-top: 5px">Description:</label>
+            <input type="search" class="form-control" id="infos" style="color: var(--font-color); background-color: var(--color-card);">
         </div>
-    </nav>
-b
-    <!-- HTML table with student data -->
-    <table id="tableID" class="display"
-        style="width: 100%;">
-    </table>
-<script type="text/javascript">
-        // Initialize a huge dataset to 
-        // see the effects of processing
-        let dataset = [];
-        for (let i = 0; i < 250000; i++) {
-            let newArr =
-                [i, "Random Data: " + i, Math.random()];
-            dataset.push(newArr);
-        }
-  
-        // Initialize the DataTable
-        $(document).ready(function () {
-            $('#tableID').DataTable({
-  
-                // Add the data created above
-                data: dataset,
-                columns: [
-                    { title: "Index" },
-                    { title: "String Index" },
-                    { title: "Random" },
-                ],
-  
-                // Enable the processing indicator
-                // of the DataTable
-                processing: true,
-            });
-        });
-    </script>
+
+
+          <!---------------2---select country option value ---------------->
+        <div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
+            <label for="Country" style="margin-bottom: 10px; margin-top: 5px">Country :</label>
+            <select name="country" id="country" class="form-control" style="color: var(--font-color); background-color: var(--color-card);">
+                <option value="">All Countries</option>
+            </select>
+        </div>
+
+
+          <!---------------3---select-seller-data-value---------------->
+        <div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
+            <label for="seller" style="margin-bottom: 10px; margin-top: 5px">Seller :</label>
+            <select name="seller" id="seller" class="form-control" style="color: var(--font-color); background-color: var(--color-card);">
+                <option value="">All</option>
+            </select>
+        </div>
+    </div>
+
+
+
+    <!---------------4---All Seller Products---------------->
+ <div class="row m-2 pt-3 " style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
+        <div class="col-sm-12 table-responsive">
+            <table id="dataTables" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);">
+                <thead>
+                    <tr>
+                        <th data-priority="1"></th>
+                        <th class="all">ID</th>
+                        <th data-priority="3">Country</th>
+                        <th data-priority="6">Description</th>
+                        <th data-priority="7">Email N</th>
+                        <th data-priority="8">Seller</th>
+                        <th data-priority="2">Proof</th>
+                        <th data-priority="9">Price</th>
+                        <th data-priority="10">created date </th>
+                        <th class="all">Buy</th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th data-priority="1"></th>
+                        <th class="all">ID</th>
+                        <th data-priority="3">Country</th>
+                        <th data-priority="6">Description</th>
+                        <th data-priority="7">Email N</th>
+                        <th data-priority="8">Seller</th>
+                        <th data-priority="2">Proof</th>
+                        <th data-priority="9">Price</th>
+                        <th data-priority="10">Added on </th>
+                        <th class="all">Buy</th>
+                    </tr>
+                  </tfoot>
+             </table>
+        </div>
+    </div>
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
+        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-notify modal-success" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <p class="heading" id="myModalHeader"></p>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true" class="white-text">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body" id="modelbody">
+                </div>
+                <div class="modal-footer justify-content-center">
+                    <a type="button" class="btn btn-outline-success waves-effect" data-dismiss="modal">Close</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="modalConfirmBuy" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-sm modal-notify modal-info" role="document">
+
+            <div class="modal-content text-center">
+
+                <div class="modal-header d-flex justify-content-center">
+                    <p class="heading">Are you sure?</p>
+                </div>
+
+                <div class="modal-body">
+                    <i class='fas fa-shopping-cart fa-4x animated rotateIn'></i>
+                </div>
+
+                <div class="modal-footer flex-center">
+                    <a onClick='confirmbye()' class="btn btn-outline-info waves-effect" data-dismiss="modal">Yes</a>
+                    <a type="button" class="btn btn-info" data-dismiss="modal">No</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="modal fade top" id="modalCoupon" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
+        <div class="modal-dialog modal-frame modal-top modal-notify modal-danger" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <div class="row d-flex justify-content-center align-items-center">
+                        <img src="layout/images/balance.png">
+                        <span class="pt-3 mx-4" style="font-size: 14 px"><b>No enough balance !</b> Please refill your balance</span>
+                        <a type="button" href="addBalance" onclick="window.open(this.href);return false;" class="btn btn-danger">Add Balance
+                                 <i class="fas fa-book ml-1 white-text"></i></a>
+                        <a type="button" class="btn btn-outline-danger waves-effect" data-dismiss="modal">No, thanks</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
- </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+</html>
 
 
