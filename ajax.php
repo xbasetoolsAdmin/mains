724b082585021d13d1f2ee54d46c47c7b45e5f7d
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-31 13:46:49 +0100

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -16,7 +16,7 @@ $r1=mysqli_num_rows($s1);
 
 $myObj =new stdClass();
 $myObj->tickets = "$r1";
-$
+$myObj->tickets = "$r1";
         $q = mysqli_query($dbcon, "SELECT resseller FROM users WHERE username='$usrid'");
           $r = mysqli_fetch_assoc($q);
 		  $reselerif = $r['resseller'];
