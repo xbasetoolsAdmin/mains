dfd8490ad8277b72da9ed778e5a244091c9c0391
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-31 15:52:49 +0100

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -1,10 +1,10 @@
 <?php ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if(!isset($_SESSION['sname']) and !isset($_SESSION['spass'])){   header("location: ../");   exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
-$s1 = mysqli_query($dbcon, "SELECT * FROM ticket where status='1' and uid='$usrid'");
-$r1=mysqli_num_rows($s1);
+$s10= mysqli_query($dbcon, "SELECT * FROM accounts WHERE sold='0'");
+$r10=mysqli_num_rows($s10);
 
 
 $row =new stdClass();
-$row->premium = "$r10";
+$myJSON->premium = "$r10";
 
 
 $q = mysqli_query($dbcon, "SELECT reseller FROM users WHERE username='$usrid'");$r = mysqli_fetch_assoc($q);  $reselerif = $r['resseller']; if ($reselerif == "1") {           $q = mysqli_query($dbcon, "SELECT soldb FROM resseller WHERE username='$usrid'");          $r = mysqli_fetch_assoc($q); $seller = $r['soldb']; $myObj->seller = "$$seller";		 	   } else { }
@@ -12,14 +12,29 @@ $myJSON = json_encode($myObj);
 
 echo $myJSON;
 ?>
-//
 <?php ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if(!isset($_SESSION['sname']) and !isset($_SESSION['spass'])){   header("location: ../");   exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
-$s1 = mysqli_query($dbcon, "SELECT * FROM ticket where status='1' and uid='$usrid'");
-$r1=mysqli_num_rows($s1);
+$s10= mysqli_query($dbcon, "SELECT * FROM accounts WHERE sold='0'");
+$r10=mysqli_num_rows($s10);
 
 
 $row =new stdClass();
-$row->premium = "$r10";
+$myJSON->premium = "$r10";
+
+
+$q = mysqli_query($dbcon, "SELECT reseller FROM users WHERE username='$usrid'");$r = mysqli_fetch_assoc($q);  $reselerif = $r['resseller']; if ($reselerif == "1") {           $q = mysqli_query($dbcon, "SELECT soldb FROM resseller WHERE username='$usrid'");          $r = mysqli_fetch_assoc($q); $seller = $r['soldb']; $myObj->seller = "$$seller";		 	   } else { }
+$myJSON = json_encode($myObj);
+
+echo $myJSON;
+?>
+
+
+<?php ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if(!isset($_SESSION['sname']) and !isset($_SESSION['spass'])){   header("location: ../");   exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
+$s10= mysqli_query($dbcon, "SELECT * FROM accounts WHERE sold='0'");
+$r10=mysqli_num_rows($s10);
+
+
+$row =new stdClass();
+$myJSON->premium = "$r10";
 
 
 $q = mysqli_query($dbcon, "SELECT reseller FROM users WHERE username='$usrid'");$r = mysqli_fetch_assoc($q);  $reselerif = $r['resseller']; if ($reselerif == "1") {           $q = mysqli_query($dbcon, "SELECT soldb FROM resseller WHERE username='$usrid'");          $r = mysqli_fetch_assoc($q); $seller = $r['soldb']; $myObj->seller = "$$seller";		 	   } else { }
