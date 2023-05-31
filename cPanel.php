59f519630483d70f02d60b17d852b5cf98e2059f
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-31 12:49:25 -0700

    Update cPanel.php

--- cPanel.php
+++ cPanel.php
@@ -1,41 +1,130 @@
-<?php 
-ob_start();
-session_start();
-date_default_timezone_set('UTC');
-require "header.php";
-include "includes/config.php";
-if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
-header("location: ../");
-exit(); }
-$usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
-
-<ul>
-            
-            
- </ul>
-</div>
-<style>.display td{background:var(--color-card);color:var(--font-color)}.dataTables_wrapper .dataTables_paginate .paginate_button{color:var(--font-color)}#accounts_data_paginate .paginate_button{color:var(--font-color)}.alert-info{color:var(--color-info);background-color:var(--color-backinfo);border-color:var(--color-borderinfo)}#accounts_data_filter{color:var(--font-color)}#accounts_data_length{color:var(--font-color)}#accounts_data_paginate{color:var(--font-color)}#accounts_data_info{color:var(--font-color)}</style>
-
-<div style="padding-center:20px; display:inline-block; margin-bottom:10px">
+ <div class="alert alert-info text-left" role="alert" style="margin: 15px;">
+        <ul>
+            <li>We Have Fresh E-mails Lists for Diffrents Countries ( You Can Use it for Spam ). </li>
+            <li>Our E-mails Lists Guarante for you Good results because most of them are from Shops and Big Sites .</li>
+            <li>Combo use for cracking (Brute forcing attack) not use for login to email.</li>
+            <li>There is <b> 0 </b> Available.</li>
+        </ul>
+    </div>
 
 
-</div>
+   <!---------------- input search ---------------->
+    <input type=hidden id="type" name="type" value="5" />
+    <div class="row m-3 pt-1" style="color: var(--font-color);">
+        <div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
+          
+            <label for="infos" style="margin-bottom: 10px; margin-top: 5px">Description:</label>
+            <input type="search" class="form-control" id="infos" style="color: var(--font-color); background-color: var(--color-card);">
+        </div>
 
- 
- 
-            <div class="row m-2 pt-3" style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
-                <div class="col-sm-12 table-responsive">
-                        </div>
-                   <table id="mainDiv" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);" ">
-               
-                </table>
+
+          <!---------------2---select country option value ---------------->
+        <div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
+            <label for="Country" style="margin-bottom: 10px; margin-top: 5px">Country :</label>
+            <select name="country" id="country" class="form-control" style="color: var(--font-color); background-color: var(--color-card);">
+                <option value="">All Countries</option>
+            </select>
+        </div>
+
+
+          <!---------------3---select-seller-data-value---------------->
+        <div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
+            <label for="seller" style="margin-bottom: 10px; margin-top: 5px">Seller :</label>
+            <select name="seller" id="seller" class="form-control" style="color: var(--font-color); background-color: var(--color-card);">
+                <option value="">All</option>
+            </select>
+        </div>
+    </div>
+
+
+
+    <!---------------4---All Seller Products---------------->
+ <div class="row m-2 pt-3 " style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
+        <div class="col-sm-12 table-responsive">
+            <table id="dataTables" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);">
+                <thead>
+                    <tr>
+                        <th data-priority="1"></th>
+                        <th class="all">ID</th>
+                        <th data-priority="3">Country</th>
+                        <th data-priority="6">Description</th>
+                        <th data-priority="7">Email N</th>
+                        <th data-priority="8">Seller</th>
+                        <th data-priority="2">Proof</th>
+                        <th data-priority="9">Price</th>
+                        <th data-priority="10">created date </th>
+                        <th class="all">Buy</th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th data-priority="1"></th>
+                        <th class="all">ID</th>
+                        <th data-priority="3">Country</th>
+                        <th data-priority="6">Description</th>
+                        <th data-priority="7">Email N</th>
+                        <th data-priority="8">Seller</th>
+                        <th data-priority="2">Proof</th>
+                        <th data-priority="9">Price</th>
+                        <th data-priority="10">Added on </th>
+                        <th class="all">Buy</th>
+                    </tr>
+                  </tfoot>
+             </table>
+        </div>
+    </div>
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
+        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-notify modal-success" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <p class="heading" id="myModalHeader"></p>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true" class="white-text">&times;</span>
+                    </button>
                 </div>
-          </div>
-     </div>
+                <div class="modal-body" id="modelbody">
+                </div>
+                <div class="modal-footer justify-content-center">
+                    <a type="button" class="btn btn-outline-success waves-effect" data-dismiss="modal">Close</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="modalConfirmBuy" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-sm modal-notify modal-info" role="document">
 
-<?php require"footer.php";?>
-<script type="text/javascript">
-$(document).keydown((function(event){"17"==event.which&&(cntrlIsPressed=!0)})),$(document).keyup((function(){cntrlIsPressed=!1}));var cntrlIsPressed=!1;function setTooltip(btn,message){$(btn).tooltip("hide").attr("data-original-title",message).tooltip("show")}function hideTooltip(btn){setTimeout((function(){$(btn).tooltip("hide")}),1e3)}$(window).on("popstate",(function(e){location.replace(document.location)})),$(window).on("load",(function(){$(".dropdown").hover((function(){$(".dropdown-toggle",this).trigger("click")})),pageDiv(7,"Accounts - xBaseTools","premium",1),new Clipboard(".copyit").on("success",(function(e){setTooltip(e.trigger,"Copied!"),hideTooltip(e.trigger),e.clearSelection()}))}));
- </script>
+            <div class="modal-content text-center">
+
+                <div class="modal-header d-flex justify-content-center">
+                    <p class="heading">Are you sure?</p>
+                </div>
+
+                <div class="modal-body">
+                    <i class='fas fa-shopping-cart fa-4x animated rotateIn'></i>
+                </div>
+
+                <div class="modal-footer flex-center">
+                    <a onClick='confirmbye()' class="btn btn-outline-info waves-effect" data-dismiss="modal">Yes</a>
+                    <a type="button" class="btn btn-info" data-dismiss="modal">No</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="modal fade top" id="modalCoupon" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
+        <div class="modal-dialog modal-frame modal-top modal-notify modal-danger" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <div class="row d-flex justify-content-center align-items-center">
+                        <img src="layout/images/balance.png">
+                        <span class="pt-3 mx-4" style="font-size: 14 px"><b>No enough balance !</b> Please refill your balance</span>
+                        <a type="button" href="addBalance" onclick="window.open(this.href);return false;" class="btn btn-danger">Add Balance
+                                 <i class="fas fa-book ml-1 white-text"></i></a>
+                        <a type="button" class="btn btn-outline-danger waves-effect" data-dismiss="modal">No, thanks</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
