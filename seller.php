10e01574302dc2d316d53b39e593d6c1723faab1
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-31 12:20:11 +0100

    Update seller.php

--- seller.php
+++ seller.php
@@ -2,21 +2,7 @@
 include'header.php';
 ?>
 
-<?php
-										
-										if ( $row[ 'type' ] == "account" )
-																				{
-																				$itemid = $row[ 's_id' ];
-																				$qe = mysqli_query( $dbcon, "SELECT * FROM accounts WHERE id='$itemid'" ) or die( mysql_error() );
-																				while ( $rowe = mysqli_fetch_assoc( $qe ) )
-																														{
-																														$country     = $rowe[ 'country' ];
-																														$site        = $rowe[ 'sitename' ];
-																														$description = $rowe[ 'infos' ];
-																														$information = $rowe[ 'url' ];
-																														$code        = array_search( "$country", $countrycodes );
-																														$countrycode = strtolower( $code );
-?>
+
 <center<h4>PREMIUM</h4></center>
 
 <table id="lead_data" class="display" style="width:100%">
