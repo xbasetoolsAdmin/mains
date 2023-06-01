ab9be4bd6eb5f563ac8026a02d330ba8873dfdc8
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-06-01 01:18:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -132,62 +132,6 @@
             </div>
         </div>
     </div>
-   <script>
- 
- 
-  <script>
- 
-      /* Initialization of datatables */
-      $(document).ready(function () {
- 
-        // Paging and other information are
-        // disabled if required, set to true
-        var myTable = $("#dataTables").DataTable({
-          paging: false,
-          searching: true,
-          info: false,
-        });
- 
-        // 2d array is converted to 1D array
-        // structure the actions are
-        // implemented on EACH column
-        myTable
-          .columns()
-          .flatten()
-          .each(function (colID) {
- 
-            // Create the select list in the
-            // header column header
-            // On change of the list values,
-            // perform search operation
-            var mySelectList = $("<select />")
-              .appendTo(myTable.column(colID).header())
-              .on("change", function () {
-                myTable.column(colID).search($(this).val());
- 
-                // update the changes using draw() method
-                myTable.column(colID).draw();
-              });
- 
-            // Get the search cached data for the
-            // first column add to the select list
-            // using append() method
-            // sort the data to display to user
-            // all steps are done for EACH column
-            myTable
-              .column(colID)
-              .cache("search")
-              .sort()
-              .each(function (param) {
-                mySelectList.append(
-                  $('<option value="' + param + '">'
-                    + param + "</option>")
-                );
-              });
-          });
-      });
-  </script>
-   
    
 </body>
 </html>
