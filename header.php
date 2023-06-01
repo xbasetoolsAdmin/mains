7ce125c7fb85260d7cfdae7a4812821cca1b923d
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-05-31 17:07:35 -0700

    Update header.php

--- header.php
+++ header.php
@@ -2,31 +2,21 @@
 ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) { header("location: ../"); exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 ?>. 
 
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.6/css/responsive.dataTables.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.4/css/buttons.dataTables.min.css">
-<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/responsive/2.2.6/js/dataTables.responsive.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.colVis.min.js"></script>
-<script src="js/jquery.dataTables.min.js"></script>
-<link href="//cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
-
-<script async src="//www.googletagmanager.com/gtag/js?id=UA-177092549-1"></script>
-<script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('set', {'$usrid': 'USER_ID'}); // Set the user ID using signed-in user_id.
-        gtag('config', 'UA-177092549-1');
-        </script>
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" href="layout/css/bootstrap.min.css">
+<script src="layout/js/jquery-3.4.1.min.js"></script>
+<script src="layout/js/clipboard.min.js"></script>
+<script src="layout/js/bootstrap.min.js"></script>
+<script src="layout/js/bootbox.min.js"></script>
 <link rel="stylesheet" href="layout/css/all.min.css" />
 <link rel="stylesheet" href="layout/css/main.css?v=12.9" />
 <link rel="stylesheet" href="layout/css/util.css" />
 <style>body{padding-top:80px}</style>
-<link rel="stylesheet" href="layout/fonts/iconic/css/material-design-iconic-font.min.css">
+<script src="js/jquery.dataTables.min.js"></script>
 <script src="layout/js/main.js"></script>
+<link rel="stylesheet" type="text/css" href="layout/css/flags.css" />
+	 
 <script type="text/javascript">
             // Notice how this gets configured before we load Font Awesome
             window.FontAwesomeConfig = { autoReplaceSvg: false }
@@ -37,8 +27,24 @@ ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes
             {
             margin:0 !important
             }
-        </style>
-</head>
+        </style>	
+<link rel="stylesheet" href="layout/fonts/iconic/css/material-design-iconic-font.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.6/css/responsive.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.4/css/buttons.dataTables.min.css">
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.2.6/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.colVis.min.js"></script>
+<link href="//cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>AddBalance</title>	
+		
+</head>	
 <style>
 
 
@@ -110,7 +116,7 @@ ob_start(); session_start(); date_default_timezone_set('UTC'); include "includes
     color: var(--font-color);
 
 }
-#balance_data_paginate .paginate_button {
+dataTables_paginate .paginate_button {
 color: var(--font-color);
 
 }
@@ -122,16 +128,16 @@ color: var(--font-color);
 
 }
 
-#balance_data_filter{
+#dataTables_filter{
   color: var(--font-color);
 }
-#balance_data_length{
+#dataTables_length{
   color: var(--font-color);
 }
-#balance_data_paginate{
+#dataTables_paginate{
   color: var(--font-color);
 }
-#balance_data_info{
+#dataTables_info{
   color: var(--font-color);
 }
 
