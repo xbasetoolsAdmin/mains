904b7697ed9e10a795588662935a1207c9679cf7
xbasetoolsAdmin <131352262+xbasetoolsAdmin@users.noreply.github.com>
2023-06-01 11:05:05 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,5 +1,4 @@
-<?php ob_start(); session_start(); date_default_timezone_set('UTC'); 
-include "includes/config.php"; if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) { header("location: /login "); exit(); } $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']); ?>
+
 <!DOCTYPE html>
    <html>   
 <link rel="stylesheet" href="buyer/layout/css/bootstrap.min.css"><link rel="stylesheet" type="text/css" href="buyer/layout/css/flags.css" /><link rel="stylesheet" href="buyer/layout/css/main.css" />
